9c1d04e0f8d4b40878b687bdfcea16e9eb9447db
Peter Cornillon <pcornillon@me.com>
2023-04-20 14:15:55 -0400

    A number of chages to generate_weights_and_locations to make it easier to use.

--- Main_Programs/generate_weights_and_locations.m
+++ Main_Programs/generate_weights_and_locations.m
@@ -1,4 +1,4 @@
-function [weights, locations] = generate_weights_and_locations(filename)
+function generate_weights_and_locations(pattern_in, num_in_range)
 % generate_weights_and_locations - does this for inputs to fix bow-tie - PCC
 %
 % This function reads in the longitudes, latitude, regridded longitudes and
@@ -9,97 +9,121 @@ function [weights, locations] = generate_weights_and_locations(filename)
 % by the input value at iPixel, iScan.
 %
 % INPUT
-%   filename - the name of the file with the orbit of interest.
+%   pattern_in - to use in directory search as follows: ['/Volumes/Aqua-1/Fronts/MODIS_Aqua_L2/SST/2010/03/AQUA_MODIS_orbit_*_' pattern_in '*']
+%    generally it is YYYMMDDT but it could be something different in the
+%    time range.
+%   num_in_range - the number of orbits in the range found with the
+%    patter_in.
 %
 % OUTPUT
-%   weights - a 3d array of the resulting griddata value in the regridded 
-%    array. The resulting griddata values are written in this array at the
-%    location of iPixel, iScan.
-%   locations - the location array of each of the resulting griddata values 
-%    in the full array.
- 
+%  none - the results are saved in a mat file.
+%
+
+% Turn off warnings for duplicate values in griddata.
+
+id = 'MATLAB:scatteredInterpolant:DupPtsAvValuesWarnId';
+warning('off',id)
+
+% Get the list of files for the pattern passed in.
+
+filelist = dir(['/Volumes/Aqua-1/Fronts/MODIS_Aqua_L2/SST/2010/03/AQUA_MODIS_orbit_*' pattern_in '*']);
+numfiles = length(filelist);
+
+file_step = floor(numfiles / num_in_range);
+
 % Read in the data
 
-latitude = ncread(filename, 'latitude');
-regridded_latitude = ncread(filename, 'regridded_latitude');
-longitude = ncread(filename, 'longitude');
-regridded_longitude = ncread(filename, 'regridded_longitude');
-
-% % % % First, pull out the first 50 pixels of each scan line.
-% % % 
-% % % test_case = 0;
-% % % 
-% % % switch test_case
-% % %     case 0
-% % %         lat = o1.latitude;
-% % %         lon = o1.longitude;
-% % %         
-% % %         regridded_lat = o1.regridded_latitude;
-% % %         regridded_lon = o1.regridded_longitude;
-% % % 
-% % %     case 1
-% % %         lat = o1.latitude(1:51,:);
-% % %         lon = o1.longitude(1:51,:);
-% % %         
-% % %         regridded_lat = o1.regridded_latitude(1:51,:);
-% % %         regridded_lon = o1.regridded_longitude(1:51,:);
-% % %         
-% % %     case 2
-% % %         lat = o1.latitude(1:51,1:100);
-% % %         lon = o1.longitude(1:51,1:100);
-% % %         
-% % %         regridded_lat = o1.regridded_latitude(1:51,1:100);
-% % %         regridded_lon = o1.regridded_longitude(1:51,1:100);
-% % %         
-% % % end
-
-% Intialize output arrays.
-
-weights = single(nan(9,size(latitude,1),size(latitude,2)));
-locations = single(nan(9,size(latitude,1),size(latitude,2)));
-
-% Now get the weights and locations
-
-tStart = tic;
-
-for iPix=4:size(latitude,1)-4
-    tic
-    for iScan=4:size(latitude,2)-4
+for iFile=1:file_step:numfiles
+    jFile = iFile;
         
-        isi = iPix-3;
-        iei = iPix+3;
+    while 1==1
+        filename_in = [filelist(jFile).folder '/' filelist(jFile).name];
         
-        jsi = iScan-3;
-        jei = iScan+3;
+        nn = strfind(filename_in, 'orbit_');
+        filename_out = ['~/Dropbox/Data/Support_data_for_MODIS_L2_Corrections_1/weights/' filename_in(nn+7:end) '_weights'];
         
-        iso = max([1 iPix-15]);
-        ieo = min([iPix+15 size(latitude,1)]);
-        
-        jso = max([1 iScan-15]);
-        jeo = min([iScan+15 size(latitude,2)]);
-        
-        vin = zeros(7,7);
-        vin(4,4) = 1;
-        
-        vout = griddata( longitude(isi:iei,jsi:jei), latitude(isi:iei,jsi:jei), vin, regridded_longitude(iso:ieo,jso:jeo), regridded_latitude(iso:ieo,jso:jeo));
-        
-        [iot, jot] = find(vout>0);
-        sol = sub2ind(size(vout), iot, jot); 
-        
-        so = sub2ind(size(latitude), iso+iot-1, jso+jot-1); 
-        
-        [wo, wio] = sort(vout(sol), 'desc');
-                
-        num_to_process = min([9 length(sol)]);
-        % %             fprintf('More than 9 hist for (iPix, iScan) = (%i, %i)\n', iPix, iScan)
-        % %         else
-
-        for k=1:num_to_process
-            weights( k, iPix, iScan) = vout(sol(wio(k)));
-            locations( k, iPix, iScan) = so(wio(k));
+        if exist(filename_out)
+            jFile = jFile + 1;
+            
+            if jFile > numfiles
+                return
+            end
+        else
+            break
         end
     end
-    fprintf('Took %5.2f s to process for iPix = %i\n', toc, iPix)
+    
+    fprintf('Working on %s\n', filename_in)
+    % Read in the data for this file.
+            
+    latitude = ncread(filename_in, 'latitude');
+    regridded_latitude = ncread(filename_in, 'regridded_latitude');
+    longitude = ncread(filename_in, 'longitude');
+    regridded_longitude = ncread(filename_in, 'regridded_longitude');
+    
+    % Make sure that there are not nan's in the input.
+    
+    nn = find(isnan(latitude)==1);
+    if ~isempty(nn)
+        fprintf('Sorry but nans in this file; griddata does not like this. Returning\n')
+        weights = nan;
+        locations = nan;
+        return
+    end
+    
+    % Intialize output arrays.
+    
+    weights = single(nan(9,size(latitude,1),size(latitude,2)));
+    locations = single(nan(9,size(latitude,1),size(latitude,2)));
+    
+    % Now get the weights and locations
+    
+    tStart = tic;
+    
+    for iPix=4:size(latitude,1)-4
+        tic
+        for iScan=4:size(latitude,2)-4
+            
+            isi = iPix-3;
+            iei = iPix+3;
+            
+            jsi = iScan-3;
+            jei = iScan+3;
+            
+            iso = max([1 iPix-15]);
+            ieo = min([iPix+15 size(latitude,1)]);
+            
+            jso = max([1 iScan-15]);
+            jeo = min([iScan+15 size(latitude,2)]);
+            
+            vin = zeros(7,7);
+            vin(4,4) = 1;
+            
+            vout = griddata( longitude(isi:iei,jsi:jei), latitude(isi:iei,jsi:jei), vin, regridded_longitude(iso:ieo,jso:jeo), regridded_latitude(iso:ieo,jso:jeo));
+            
+            [iot, jot] = find(vout>0);
+            sol = sub2ind(size(vout), iot, jot);
+            
+            so = sub2ind(size(latitude), iso+iot-1, jso+jot-1);
+            
+            [wo, wio] = sort(vout(sol), 'desc');
+            
+            num_to_process = min([9 length(sol)]);
+            % %             fprintf('More than 9 hist for (iPix, iScan) = (%i, %i)\n', iPix, iScan)
+            % %         else
+            
+            for k=1:num_to_process
+                weights( k, iPix, iScan) = vout(sol(wio(k)));
+                locations( k, iPix, iScan) = so(wio(k));
+            end
+        end
+        fprintf('Took %5.2f s to process for iPix = %i\n', toc, iPix)
+    end
+    
+    fprintf('Took %f s to process for the entire run.\n', toc(tStart))
+    
+    % And save the results
+    
+    save( filename_out, 'filename_in', 'weights', 'locations')
+    clear weights locations
 end
-
-fprintf('Took %f s to process for the entire run.\n', toc(tStart))

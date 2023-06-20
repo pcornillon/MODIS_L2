72f105a0f24a92f223fba7a8127af577f0039ef4
pcornillon <pcornillon@me.com>
2023-06-20 14:05:17 -0400

    Fleshed out show_fix_bowtie and made it a function called from show_fix_bowtie_control

--- Diagnostic_Programs/show_fix_bowtie.m
+++ Diagnostic_Programs/show_fix_bowtie.m
@@ -1,138 +1,314 @@
-% show_fix_bowtie - demo plots of impact of bow-tie and fixed version.
-%
-% Read sst input masked, fast regridded version and griddata version for an
-% obit. Determine the gradient magnitudes of these, then plot them and
-% histogram the differences.
-%
+function show_fix_bowtie
+% show_fix_bowtie - function that plots the actual bow-tie stuff - PCC
+%  
 
-fi = '~/Dropbox/Data/Fronts_test/MODIS_Aqua_L2/SST/test4/2005/09/AQUA_MODIS_orbit_17970_20050919T054735_L2_SST.nc4';
-ncdisp(fi)
-sst_in = ncread( fi, 'SST_In');
-dbquit
+% Define global variables
+
+global fi
+global AXIS CAXIS_sst CAXIS_sst_diff_1 CAXIS_sst_diff_2 CAXIS_gm CAXIS_gm_diff
+global XLIM HIST_SST HIST_GM_1 HIST_GM_2 HIST_GM_3 COUNTS_LIM ZOOM_RANGE
+global title_fontsize axis_fontsize
+global along_scan_seps_array along_track_seps_array
+
+% Get sst data
+
 sst_in = ncread( fi, 'SST_In_Masked');
-sst_fast = ncread( fi, 'regridded_sst');
 sst_griddata = ncread( fi, 'regridded_sst_alternate');
-dd_in_fast = sst_in - sst_fast;
-dd_fast_griddata = sst_fast - sst_griddata;
-imagesc(dd_in_fast')
-set(gca,fontsize=24)
-title('In - Fast', fontsize=30)
-figure
-imagesc(dd_fast_griddata')
-set(gca,fontsize=24)
-title('Fast- Griddata', fontsize=30)
-axis([1000 1354 6870 7000])
-caxis([-.1 .1])
-[min(dd_fast_griddata(:))]
-[min(dd_fast_griddata(:)) max(dd_fast_griddata(:))]
-axis tight
-caxis([-.1 .1])
-colorbar
+
+dd_in_griddata = sst_in - sst_griddata;
+
+% And get the gradient magnitudes and zoom in on the area of interest.
+
+[~, ~, gm_in] = sobel_gradient_degrees_per_kilometer( ...
+    sst_in, ...
+    along_track_seps_array(:,1:size(sst_in,2)), ...
+    along_scan_seps_array(:,1:size(sst_in,2)));
+
+gm_in_z = gm_in(ZOOM_RANGE(1):ZOOM_RANGE(2), ZOOM_RANGE(3):ZOOM_RANGE(4));
+
+[~, ~, gm_griddata] = sobel_gradient_degrees_per_kilometer( ...
+    sst_griddata, ...
+    along_track_seps_array(:,1:size(sst_griddata,2)), ...
+    along_scan_seps_array(:,1:size(sst_griddata,2)));
+
+gm_griddata_z = gm_griddata(ZOOM_RANGE(1):ZOOM_RANGE(2), ZOOM_RANGE(3):ZOOM_RANGE(4));
+
+
+% Get list of orbits with fixed weights and locations
+
+filelist = dir('~/Dropbox/Data/Support_data_for_MODIS_L2_Corrections_1/*fixed.mat*');
+
+% Loop over this list fast regridding using the weights and locations from
+% the orbits with them.
+
+iFig = 0;
+
+for iFile=1:length(filelist)
+    filename = [filelist(iFile).folder '/' filelist(iFile).name];
+    
+    nn = strfind(filelist(iFile).name, 'weights_and_locations_');
+    orbit_no = filelist(iFile).name(nn+22:nn+26);
+    
+    fprintf('\n\n%i) Results for orbit number %s\n\n', iFile, orbit_no)
+    
+    % Get the weights and locations for this orbit.
+    
+    load(filename)
+    
+    % Regrid with this set of weights and locations
+    
+    [nElements, nScans] = size(sst_in);
+    [nMax, mElements, mScans] = size(augmented_weights);
+    
+    % Truncate weights array to same number of scan lines as SST array.
+    
+    weights = augmented_weights(:,:,1:nScans);
+    locations = augmented_locations(:,:,1:nScans);
+    
+    % Now regrid.
+    
+    sst_fast = zeros([nElements, nScans]);
+    
+    for iC=1:nMax
+        weights_temp = squeeze(weights(iC,:,:));
+        locations_temp = squeeze(locations(iC,:,:));
+        
+        non_zero_weights = find(weights_temp ~= 0);
+        
+        SST_temp = zeros([nElements, nScans]);
+        SST_temp(non_zero_weights) = weights_temp(non_zero_weights) .* sst_in(locations_temp(non_zero_weights));
+        
+        sst_fast = sst_fast + SST_temp;
+    end
+    
+    %% Now get stats and plot
+    
+    dd_in_fast = sst_in - sst_fast;
+    dd_fast_griddata = sst_fast - sst_griddata;
+    
+%     % Plot sst differences betwee sst in and fast iterpolated version.
+%     figure(1)
+%     clf
+%     
+%     subplot(2,2,1)
+%     
+%     imagesc(sst_in')
+%     set(gca,fontsize=axis_fontsize)
+%     title('SST_{in}', fontsize=title_fontsize)
+%     axis(AXIS)
+%     caxis(CAXIS_sst)
+%     colorbar
+%     
+%     subplot(2,2,2)
+%     
+%     imagesc(sst_fast')
+%     set(gca,fontsize=axis_fontsize)
+%     title('SST_{fast}', fontsize=title_fontsize)
+%     axis(AXIS)
+%     caxis(CAXIS_sst)
+%     colorbar
+%     
+%     subplot(2,2,3)
+%     
+%     imagesc(dd_in_fast')
+%     set(gca,fontsize=axis_fontsize)
+%     title('In - Fast', fontsize=title_fontsize)
+%     axis(AXIS)
+%     caxis(CAXIS_sst_diff_1)
+%     colorbar
+%     
+%     % Plot sst differences betwee fast iterpolated and griddata version.
+%     subplot(2,2,4)
+%     
+%     imagesc(dd_fast_griddata')
+%     set(gca,fontsize=axis_fontsize)
+%     title('Fast - Griddata', fontsize=title_fontsize)
+%     axis(AXIS)
+%     caxis(CAXIS_sst_diff_2)
+%     colorbar
+    
+    % Histogram differences
+    iFig = iFile * 10 + 1;
+    figure(iFig)
+    clf
+    
+    hh = histogram(dd_in_fast, HIST_SST);
+    hold on
+    hh2 = histogram(dd_fast_griddata, HIST_SST);
+    
+    set(gca,fontsize=axis_fontsize)
+    ylim([0 100000])
+    xlabel('^\circ C')
+    ylabel('Counts')
+    xlim(XLIM)
+    title(['SST Differences for Orbit ' num2str(orbit_no)], fontsize=title_fontsize)
+    
+    % Stats on sst differences
+    
+    sigma_in_fast = std(dd_in_fast(:),'omitnan');
+    sigma_fast_griddata = std(dd_fast_griddata(:),'omitnan');
+    fprintf('100 * sigma_fast / sigma_griddata %3.0f%%\n', 100 * sigma_fast_griddata / sigma_in_fast)
+    
+    % Now get gradient magnitudes for each of the fields.
+    
+    [~, ~, gm_fast] = sobel_gradient_degrees_per_kilometer( ...
+        sst_fast, ...
+        along_track_seps_array(:,1:size(sst_fast,2)), ...
+        along_scan_seps_array(:,1:size(sst_fast,2)));
+    
+    % Get gradient magnitudes for zoomed in region
+    
+    gm_fast_z = gm_fast(ZOOM_RANGE(1):ZOOM_RANGE(2), ZOOM_RANGE(3):ZOOM_RANGE(4));
+
+    % Plot gradients
+    iFig = iFile * 10 + 2;
+    figure(iFig)
+    clf
+    
+    subplot(2,2,1)
+    
+    imagesc(gm_in_z')
+    colorbar
+    set(gca,fontsize=axis_fontsize)
+    title('|\nabla SST_{in}|', fontsize=title_fontsize, interpreter='tex')
+%     axis(AXIS)
+    caxis(CAXIS_gm)
+    colorbar
+    
+    subplot(2,2,2)
+    
+    imagesc((gm_griddata_z - gm_fast_z)')
+    colorbar
+    set(gca,fontsize=axis_fontsize)
+    title('|\nabla SST_{griddata}| - |\nabla SST_{fast}|', fontsize=title_fontsize, interpreter='tex')
+%     axis(AXIS)
+    caxis(CAXIS_gm_diff)
+    colorbar
+    
+    subplot(2,2,3)
+    imagesc(gm_griddata_z')
+    set(gca,fontsize=axis_fontsize)
+    title('|\nabla SST_{griddata}|', fontsize=title_fontsize, interpreter='tex')
+%     axis(AXIS)
+    caxis(CAXIS_gm)
+    colorbar
+    
+    subplot(2,2,4)
+    imagesc(gm_fast_z')
+    set(gca,fontsize=axis_fontsize)
+    title('|\nabla SST_{fast}|', fontsize=title_fontsize, interpreter='tex')
+%     axis(AXIS)
+    caxis(CAXIS_gm)
+    colorbar
+      
+    sgtitle(['Gradient Magnitudes for Orbit ' num2str(orbit_no)], fontsize=30)
+    
+    % And differences between sst in and the fast interpolation.
+    dd_gm_in_fast = gm_in_z - gm_fast_z;
+    dd_gm_fast_griddata = gm_fast_z - gm_griddata_z;
+    
+    % Histogram the differences between gradient magnitudes.
+    iFig = iFile * 10 + 3;
+    figure(iFig)
+    clf
+    
+    hhgm1 = histogram(dd_gm_in_fast, HIST_GM_1);
+    hold on
+    hhgm2 = histogram(dd_gm_fast_griddata, HIST_GM_2);
+    legend({'|\nabla{SST\_in}| - |\nabla{SST\_fast}|' '|\nabla{SST\_fast}| - |\nabla{SST\_griddata}|'})
+    set(gca,fontsize=axis_fontsize)
+    xlabel('K/km')
+    ylabel('Counts')
+    xlim(XLIM)
+    title(['Gradient Magnitude Differences for Orbit ' num2str(orbit_no)], fontsize=title_fontsize)
+    
+    % And stats for these difference.
+    sigma_gm_in_fast = std(dd_gm_in_fast(:),'omitnan');
+    sigma_gm_fast_griddata = std(dd_gm_fast_griddata(:),'omitnan');
+    
+    fprintf('100 * sigma_gm_in_fast / sigma_gm_fast_griddata = %3.0f%%\n', 100 * sigma_gm_fast_griddata / sigma_gm_in_fast)
+
+    %% Next histogram gradients for various parts of the distance from nadir
+    iFig = iFile * 10 + 4;
+    figure(iFig)
+    clf
+    
+    subplot(131)
+    
+    xx1 = gm_in(1:300,:);
+    xx2 = gm_fast(1:300,:);
+    mm = find(xx1>0 & xx2>0);
+    histogram(xx1(mm), [0:0.01:0.6])
+    hold on
+    histogram(xx2(mm), [0:0.01:0.6])
+    
+    set(gca,fontsize=axis_fontsize)
+    xlabel('K/km')
+    ylabel('Counts')
+    title( 'Pixels 1-300', fontsize=title_fontsize)
+    legend({'|\nabla{SST\_in}|' '|\nabla{SST\_fast}|'})
+    
+    subplot(132)
+    
+    xx1 = gm_in(677-150:677+150,:);
+    xx2 = gm_fast(677-150:677+150,:);
+    mm = find(xx1>0 & xx2>0);
+    histogram(xx1(mm), [0:0.01:0.6])
+    hold on
+    histogram(xx2(mm), [0:0.01:0.6])
+    
+    set(gca,fontsize=axis_fontsize)
+    xlabel('K/km')
+    ylabel('Counts')
+    title( ['Pixels ' num2str(677-150) '-' num2str(677+150)], fontsize=title_fontsize)
+    legend({'|\nabla{SST\_in}|' '|\nabla{SST\_fast}|'})
+    
+    subplot(133)
+    
+    xx1 = gm_in(1054:1354,:);
+    xx2 = gm_fast(1054:1354,:);
+    mm = find(xx1>0 & xx2>0);
+    histogram(xx1(mm), [0:0.01:0.6])
+    hold on
+    histogram(xx2(mm), [0:0.01:0.6])
+    
+    set(gca,fontsize=axis_fontsize)
+    xlabel('K/km')
+    ylabel('Counts')
+    title( 'Pixels 1054-1354', fontsize=title_fontsize)
+    legend({'|\nabla{SST\_in}|' '|\nabla{SST\_fast}|'})
+    
+    sgtitle(['Gradient Magnitudes for Orbit ' num2str(orbit_no)], fontsize=30)
+
+    %% Now plot all of the fast-griddata histograms in one plot.
+    
+    figure(1)
+    subplot(2,3,iFile)
+    
+    histogram(dd_gm_fast_griddata, HIST_GM_3)
+    
+    set(gca,fontsize=axis_fontsize)
+    xlabel('K/km')
+    ylabel('Counts')
+    ylim(COUNTS_LIM)
+    title(['Orbit ' num2str(orbit_no)], fontsize=title_fontsize)
+
+    % And all of the fast-griddata fields in one plot.
+    
+    figure(2)
+    subplot(2,3,iFile)
+    
+    imagesc((gm_griddata_z - gm_fast_z)')   
+    set(gca,fontsize=axis_fontsize)
+    title(['Orbit ' num2str(orbit_no)], fontsize=title_fontsize)
+    colorbar
+    caxis(CAXIS_gm_diff)    
+end
+
 figure(1)
-caxis([-.1 .1])
-colorbar
-map_axis_range('a', 2, 1)
-figure
-histogram(dd_in_fast)
-ylim([])
-dbquit
-ylim([0 10000])
-ylim([0 100000])
-hh = histogram(dd_in_fast,[-1:0.01:1]);
-hold on
-hh2 = histogram(dd_fast_griddata,[-1:0.01:1]);
-ylim([0 100000])
-sst_griddata_med = medfilt2(sst_griddata);
-figure
-dd_fast_griddata_med = sst_fast - sst_griddata_med;
-imagesc(dd_fast_griddata_med')
-set(gca,fontsize=24)
-title('Fast - Griddata_med', fontsize=30)
-map_axis_range('a', 2, 4)
-set(gca,fontsize=24)
-map_axis_range('c', 2, 4)
-colorbar
-figure(3)
-hh3 = histogram(dd_fast_griddata_med,[-1:0.01:1]);
-whos *med*
-clear *med*
-hh3
-hh3.Visible='off';
-std(dd_in_fast)
-std(dd_in_fast(:),'omitnan')
-std(dd_fast_griddata(:),'omitnan')
-1.62/0.1061
-doc sobel
-[~, ~, gm_in] = Sobel( sst_in, 1);
-[~, ~, gm_fast] = Sobel( sst_fast, 1);
-[~, ~, gm_griddata] = Sobel( sst_griddata, 1);
-figure
-imagesc(gm_in')
-colorbar
-caxis([0 .1])
-caxis([0 1])
-set(gca,fontsize=24)
-title('Gradient Magnitude sst\_in', fontsize=30)
-figure
-imagesc(gm_griddata')
-imagesc(gm_fast')
-set(gca,fontsize=24)
-title('Gradient Magnitude sst\_fast', fontsize=30)
-caxis([0 1])
-figure
-imagesc(gm_griddata')
-set(gca,fontsize=24)
-title('Gradient Magnitude sst\_griddata', fontsize=30)
-caxis([0 1])
-map_axis_range('c', 2, 6)
-map_axis_range('a', 2, 6)
-caxis([0 1])
-colorbar
-caxis([0 0.5])
-map_axis_range('a', 2, 5)
-map_axis_range('a', 2, 4)
-map_axis_range('a', 6, 4)
-map_axis_range('a', 6, 5)
-colorbar
-caxis([0 0.5])
-axis([800 1354 8801 9000])
-map_axis_range('a', 4, 5)
-map_axis_range('a', 4, 6)
-axis([800 1354 8861 8940])
-figure(4)
-axis([800 1354 8861 8940])
-figure(6)
-axis([800 1354 8861 8940])
-axis([800 1354 8901 8930])
-figure(4)
-axis([800 1354 8901 8930])
-figure(4)
-axis([1250 1354 8901 8930])
-figure(4)
-axis([1220 1354 8901 8930])
-figure(5)
-axis([1220 1354 8901 8930])
-figure(6)
-axis([1220 1354 8901 8930])
-xx1 = gm_in(1221:1354, 8901:8930);
-figure
-imagesc(xx1)
-figure(7)
-imagesc(xx1')
-caxis([0 0.5])
-figure(7)
-xx_in = gm_in(1221:1354, 8901:8930);
-clear xx1
-xx_fast = gm_fast(1221:1354, 8901:8930);
-xx_griddata = gm_griddata(1221:1354, 8901:8930);
-dd = xx_in - xx_fast;
-dd_gm_in_fast = xx_in - xx_fast;
-dd_gm_fast_griddata = xx_fast - xx_griddata;
-figure
-hhgm1 = histogram(dd_gm_in_fast)
-hhgm1 = histogram(dd_gm_in_fast,[-.34:0.05:.5])
-hhgm1 = histogram(dd_gm_in_fast,[-.34:0.02:.5])
-hold on
-hhgm2 = histogram(dd_gm_fast_griddata,[-.34:0.02:.5])
-[std(dd_gm_in_fast(:),'omitnan') std(dd_gm_fast_griddata(:),'omitnan')]
-0.62/0.0771
+sgtitle('|\nabla{SST\_griddata}| - |\nabla{SST\_fast}|', fontsize=30)
+
+figure(2)
+sgtitle('|\nabla{SST\_griddata}| - |\nabla{SST\_fast}|', fontsize=30)
+
+end
--- Functions/generate_weights_for_fast_regridding_Functions/fix_weights.m
+++ Functions/generate_weights_for_fast_regridding_Functions/fix_weights.m
@@ -1,37 +1,119 @@
 % fix_weights - Replace nan values in one set of fast weights with non-nan values from another - PCC
 %
-% Use weights and location from orbit 41616 as the base. Replace nans in
-% this orbit with non-nan values from orbit 41821.
+% Get all orbits with weights and locations. Then, replace nans in all
+% obits using values from a specified orbit.
 %
 
-% Get weights & locations from 41616.
+% Get the list of orbits with weights and locations already calculated.
+filelist = dir('~/Dropbox/Data/Support_data_for_MODIS_L2_Corrections_1/weights/*reordered*');
 
-wghts_41616 = load('~/Dropbox/Data/Support_data_for_MODIS_L2_Corrections_1/weights/41616_20100301T000736_L2_SST_weights_reordered.mat');
+for iFile=1:length(filelist)
+    fprintf('%i) %s\n', iFile, filelist(iFile).name)
+end
 
-% Get weights & locations from 41821 and eliminate the last set of values.
-% Weights for the 7th layer are all essentially 0.
+ref_orbit_no = input('Above are the orbits with weights and locations, choose a reference orbit: ', 's');
 
-xx = load('~/Dropbox/Data/Support_data_for_MODIS_L2_Corrections_1/weights/41821_20100315T015843_L2_SST_weights_reordered.mat');
-wghts_41821 = xx;
-wghts_41821.weights = xx.weights(1:6,:,:);
-wghts_41821.locations = xx.locations(1:6,:,:);
+% Loop over files looking for the reference orbit
 
-clear xx
+for iFile=1:length(filelist)
+    filename = [filelist(iFile).folder '/' filelist(iFile).name];
+    
+    % Skip if this is the reference orbit.
+    nn = strfind(filelist(iFile).name, ref_orbit_no);
+    
+    if ~isempty(nn)
+        iRef = iFile;
+        
+        [ref_weights, ref_locations] = get_and_trim_weights(filename) ;       
+    end
+end
 
-% Find nans in orbit 41616.
+% Now loop over orbits, correcting them with the reference orbit.
 
-xx = squeeze(wghts_41616.weights(1,:,:));
-nn = find(isnan(wghts_41616.weights) == 1);
+for iFile=1:length(filelist)
+    filename = [filelist(iFile).folder '/' filelist(iFile).name];
+    orbit_no = filelist(iFile).name(1:5);
+    
+    fprintf('%i) Working on orbit number %s with filename %s\n', iFile, orbit_no, filelist(iFile).name)
+    
+    % Skip if this is the reference orbit.
+    nn = strfind(filename, ref_orbit_no);
+    if isempty(nn)
 
-% Replace the nans with values from orbit 41821.
+        % Get weights & locations from this orbit.
+        
+        [weights, locations] = get_and_trim_weights(filename) ;       
 
-wghts_41616_fixed.weights = wghts_41616.weights;
-wghts_41616_fixed.weights(nn) = wghts_41821.weights(nn);
-wghts_41616_fixed.locations = wghts_41616.locations;
-wghts_41616_fixed.locations(nn) = wghts_41821.locations(nn);
+        % Find nans in orbit this orbit.
+        
+        nn = find(isnan(weights) == 1);
+        
+        % Replace the nans with values from orbit 41821.
+        
+        augmented_weights = weights;
+        augmented_weights(nn) = ref_weights(nn);
+        
+        augmented_locations = locations;
+        augmented_locations(nn) = ref_locations(nn);
+        
+        % Save the fixed weights and locations for use in build_and_fix...
+        
+        save(['~/Dropbox/Data/Support_data_for_MODIS_L2_Corrections_1/weights_and_locations_' orbit_no '_fixed.mat'], 'augmented_weights', 'augmented_locations');
+    else
+        fprintf('Skipping file''s, this is the reference obit.\n', filename)
+    end
+end
 
-% Save the fixed 41616 weights and locations for use in build_and_fix...
+%% Functions
 
-agumented_weights = wghts_41616_fixed.weights;
-agumented_locations = wghts_41616_fixed.locations;
-save('~/Dropbox/Data/Support_data_for_MODIS_L2_Corrections_1/weights_and_locations_41616_fixed.mat', 'agumented_weights', 'agumented_locations');
+function [weights, locations] = get_and_trim_weights(filename)
+% get_and_trim_weights - and locations for the specified file
+%
+% Reads the file with the weights and locations and if there are more than
+% 6 levels, makes sure that the higher levels do not contribute much and
+% then, assuming that they don't trims the file.
+%
+% INPUT 
+%   filenname - name of file with data.
+%
+% OUTPUT
+%   weights - trimmed to 6 levels from input file.
+%   locations - trimmed to 6 levels from input file.
+
+% Get weights & locations for this orbit
+
+load(filename);
+[nLevels, nPixels, nScans] = size(weights);
+
+% Make sure that it is at least 6 levels of weights.
+
+if nLevels < 6
+    fprintf('%i levels. Need at least 6. Will return nan.\n', nLevels)
+    weights = nan;
+    locations = nann;
+    return
+end
+
+% Make sure that there are at most 6 levels of weights
+
+if nLevels > 6
+    fprintf('%i levels, will trim to 6.\n', nLevels)
+    
+    for iLevel=7:nLevels
+        xx = squeeze(weights(iLevel,:,:));
+        fprintf('Level %i) min/max %f/%f\n', iLevel, min(xx(:)), max(xx(:)))
+        
+        if (abs(min(xx(:))) > 0.1) | (abs(max(xx(:))) > 0.1)
+            fprinntf('Problem; min or max value exceeds 0.1\n')
+            weights = nan;
+            locations = nan;
+            return
+        end
+    end
+    
+    weights = weights(1:6,:,:);
+    locations = locations(1:6,:,:);
+end
+
+end
+

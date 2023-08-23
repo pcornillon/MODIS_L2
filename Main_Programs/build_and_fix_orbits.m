7f0a5ac3afa63c33f17a0d47ef89d11bf11e9f47
Peter Cornillon <pcornillon@me.com>
2023-08-23 13:41:27 -0400

    Another change to use .mat instead of .n4 file.

--- Main_Programs/build_and_fix_orbits.m
+++ Main_Programs/build_and_fix_orbits.m
@@ -384,7 +384,7 @@ end
 % Gradient stuff
 
 if get_gradients
-    gradient_filename = [fixit_directory 'Separation_and_Angle_Arrays.n4'];
+    gradient_filename = [fixit_directory 'Separation_and_Angle_Arrays.mat'];
     
     % track_angle = ncread( gradient_filename, 'track_angle');
     load(gradient_filename)

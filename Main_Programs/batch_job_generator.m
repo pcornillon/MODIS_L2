70da556a8d214e804d50c8108836f208f686ab51
Peter Cornillon <pcornillon@me.com>
2023-10-19 13:36:33 -0400

    Added script to generate a number of batch jobs.

---
+++ Main_Programs/batch_job_generator.m
@@ -0,0 +1,34 @@
+% batch_job_generator - used to submit a number of build_and_fix_orbit jobs - PCC
+
+Option = 4;
+save_orbits = 1;
+
+fprintf('\nYou will be asked to enter a information needed to submit a number of batch jobs.\n')
+fprintf('For each input requested there will be an example. The example will submit 5 batch jobs\n')
+fprintf('with the first job starting with data on 10 February 2010 at 2 AM. This job will process\n')
+fprintf('6 hours worth of data; i.e., from [2010 2 10 2 0 0] to [2010 2 10 8 0 0]. The start of the\n')
+fprintf('second jobwill be offset from the start of the first job by 1 day ([0 0 1 0 0 0]) and it\n')
+fprintf('will also process 6 hours of data; i.e., from [2010 2 11 2 0 0] to [2010 2 11 8 0 0].\n')
+fprintf('And so, with the fifth job processing data from [2010 2 14 2 0 0] to [2010 2 14 8 0 0]\n')
+fprintf('Don''t worry, were you to ask for 90 jobs, it will figure out the change in month and \n')
+fprintf('year if necessary. Have at it.\n\n')
+
+start_time = input('Enter the start date/time the batch jobs are to use as [yyyy mm dd hh min ss]; e.g, [2010 2 10 2 0 0]: ');
+period_to_process = input('Enter the date/time range for each batch job as [yyyy mm dd hh min ss]; e.g, [0 0 0 6 0 0] to process 6 hours worth of data: ');
+time_between_batch_jobs = input('Enter the date/time between the start of one batch times and the next as [yyyy mm dd hh min ss]; e.g, [0 0 1 0 0 0] for one day of satellite time between batch intervals: ');
+num_batch = input('Enter the number of batch jobs to submit; e.g, 5 to submit 5 batch jobs: ');
+
+fprintf('\n')
+
+mat_start_time = datenum(start_time);
+mat_period_to_process = datenum(period_to_process);
+mat_time_between_batch_jobs = datenum(time_between_batch_jobs);
+
+for iJob=1:num_batch
+    tStart = datevec(mat_start_time) + datevec(mat_time_between_batch_jobs) * (iJob - 1);
+    tEnd = tStart + datevec(mat_period_to_process);
+
+    fprintf('Submitting job to process from %s to %s\n', datestr(tStart), datestr(tEnd))
+
+    % job_number(iJob) = batch( 'build_wrapper', 0, {Option, tStart, tEnd, save_orbits}, CaptureDiary=true);
+end
---
+++ Main_Programs/rename_NRT_files.m
@@ -0,0 +1,58 @@
+% rename_NRT_files - find nrt metadata granules and rename without nrt_ - PCC
+%
+% Some of the OBPG granules have 'nrt_' in their names. The corresponding
+% data granules in NASA's AWS S3 repository do not have 'nrt_' in their
+% names. (I checked a few and the SST fields are identical.) The purpose of
+% this script is to rename these granules, removing the 'NRT_'.
+%
+
+clear renamed_granules
+
+metadata_run = 0;
+
+if metadata_run
+    base_dir = '/Volumes/Aqua-1/MODIS_R2019/Data_from_OBPG_for_PO-DAAC/';
+else
+    base_dir = '/Volumes/Aqua-1/MODIS_R2019/combined/';
+end
+
+
+
+iRenamed = 0;
+
+for iYear=2002:2021
+    YearS = num2str(iYear);
+
+    % Get the list of granules with NRT in their names for this year.
+
+    dir_in = [base_dir YearS '/'];
+    filelist = dir([dir_in 'AQUA*NRT*']);
+
+    for iFile=1:length(filelist)
+
+        file_in  = [filelist(iFile).folder '/' filelist(iFile).name];
+
+        if metadata_run
+            file_out = strrep(file_in, '_NRT', '');
+        else
+            file_out = strrep(file_in, '.NRT', '');
+        end
+
+        iRenamed = iRenamed + 1;
+        renamed_granules(iRenamed) = string(file_in);
+
+        fprintf('%i) Renaming %s to %s\n', iRenamed, file_in, file_out)
+
+        eval(['! mv -n ' file_in ' ' file_out])
+    end
+end
+
+rename_filelist = dir([base_dir 'Filelists_and_Logs/rename_log*']);
+
+if isempty(rename_filelist)
+    fileout = [base_dir 'Filelists_and_Logs/rename_log_1.txt'];
+else
+    fileout = [base_dir 'Filelists_and_Logs/rename_log_' num2str(length(deletion_filelist)+1) '.mat'];
+end
+
+save( fileout, 'iRenamed', 'renamed_granules')

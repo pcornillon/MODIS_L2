c3d9a92e53e8ad1eeb5f3dc77ecb3f26946188ab
Peter Cornillon <pcornillon@me.com>
2024-01-05 13:53:49 -0500

    Also added information in the problem list about the orbit in which the problem occurred.

--- Functions/General_Functions/populate_problem_list.m
+++ Functions/General_Functions/populate_problem_list.m
@@ -20,17 +20,35 @@ global oinfo iOrbit iGranule iProblem problem_list
 
 iProblem = iProblem + 1;
 
-problem_list(iProblem).problem_description = problem_description;
-problem_list(iProblem).code = status;
 
-if exist('granule_start_time_guess')
-    problem_list(iProblem).granule_start_time = granule_start_time_guess;
-end
+problem_list(iProblem).code = status;
+problem_list(iProblem).problem_description = problem_description;
 
 % In what function did the error occur?
 
 st = dbstack;
 problem_list(iProblem).calling_function = st(2).name;
+
+% Add info about the orbit in which this problem occurs.
+
+if iOrbit > 0
+    if ~isempty(oinfo(iOrbit).name)
+        problem_list(iProblem).orbit_name = oinfo(iOrbit).name;
+
+        if ~isempty(oinfo(iOrbit).orbit_number)
+            problem_list(iProblem).orbit_number = oinfo(iOrbit).orbit_number;
+            problem_list(iProblem).orbit_start_time = oinfo(iOrbit).start_time;
+        end
+
+        if exist('iGranule')
+            problem_list(iProblem).iGranule = oinfo(iOrbit).iGranule;
+        end
+    end
+end
+
+if exist('granule_start_time_guess')
+    problem_list(iProblem).granule_start_time = granule_start_time_guess;
+end
 
 end
 

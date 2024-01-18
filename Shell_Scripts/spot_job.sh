40ad0a886561d608f556c9901aed24d4ec8bf783
Peter Cornillon <pcornillon@me.com>
2024-01-18 16:26:51 -0500

    Modified the script to be run for spot instances to run both the Matlab portion and the AWS_copy... portion in the background and to redirect the output to the uri-nfs-cornillon location.

--- Shell_Scripts/spot_job.sh
+++ Shell_Scripts/spot_job.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # write commands to excecute here
 
+# Define the output directory for the nohup logs to be generated from the Matlab and python commands below.
+
+OUTPUT_DIRECTORY="/mnt/uri-nfs-cornillon/Logs/nohup/"
+
+# Ensure the output directory exists, if it doesn't, create it.
+
+mkdir -p "$OUTPUT_DIRECTORY"
+
 # Make sure that we are using the most recent version of MODIS_L2
 
 cd ~/Documents/MODIS_L2/
@@ -9,13 +17,19 @@ git pull
 # Start Matlab and run test script. The script it runs will exit after at least 75% (which could be changed, e.g./ to 100%) of the jobs have finished
 # or after the estimated required processing time has elapsed. It estimates this time based on the time for one of the submitted batch jobs to finish.
 # It estimates this time assuming 100 minutes per orbit and 11 minutes to process an orbit. These numbers are changeable depending on how fast the CPU is.
-# Note that the command opens the Matlab project for MODIS_L2. From there it finds all of the functions it needs.
+# Note that the command opens the Matlab project for MODIS_L2. From there it finds all of the functions it needs. First, construct the output filename
+# then execute the command
 
-nohup matlab -nodisplay -nosplash -nodesktop -r "prj=openProject('/home/ubuntu/Documents/MODIS_L2/MODIS_L2.prj'); MacStudio_AWS_batch_test_1" &
+CURRENT_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
+FILENAME="matlab_${CURRENT_TIME}.out"
+
+nohup matlab -nodisplay -nosplash -nodesktop -r "prj=openProject('/home/ubuntu/Documents/MODIS_L2/MODIS_L2.prj'); MacStudio_AWS_batch_test_1"  > "${OUTPUT_DIRECTORY}${FILENAME}" 2>&1 &
 
 # Submit Python job to copy .nc4 files from local storage to remote storage. Note that we first move to the folder with the copy script in it.
 
 cd Shell_Scripts
 
-python AWS_copy_nc4_to_remote.py  
+CURRENT_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
+FILENAME="AWS_copy_${CURRENT_TIME}.out"
+nohup python AWS_copy_nc4_to_remote.py > "${OUTPUT_DIRECTORY}${FILENAME}" log 2>&1 &
 

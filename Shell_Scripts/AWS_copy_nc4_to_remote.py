5c599e4231d202771d7e4993afe990a26c66ec0b
Peter Cornillon <pcornillon@me.com>
2024-01-19 17:04:40 -0500

    Tracked down a syntax error in a print statement that was causing a problem in the next statement.

--- Shell_Scripts/AWS_copy_nc4_to_remote.py
+++ Shell_Scripts/AWS_copy_nc4_to_remote.py
@@ -143,12 +143,9 @@ def copy_files(test_mode=False):
 
                         if print_debug:
                             # print(f'specific_ input_folder: {specific_input_folder}, output_folder: {specific_output_folder}, file_path: {file_path}, file_creation_time: {file_creation_time} and time.time: {time.time()}')
-                            # print( "specific_ input_folder: %s, output_folder: %s, file_path: %s, file_creation_time: %s and time.time: %s" %(specific_input_folder, specific_output_folder, file_path, file_creation_time, time.time())
+                            print( "specific_ input_folder: %s, output_folder: %s, file_path: %s, file_creation_time: %s and time.time: %s" %(specific_input_folder, specific_output_folder, file_path, file_creation_time, time.time()))
 
-                            tempb = time_since_creation * 60
-                            # tempa = time.time() - file_creation_time
-                            tempa = time.time()
-                            if tempa > tempb:
+                        if (time.time() - file_creation_time) > (time_since_creation * 60):
                             if print_debug:
                                 # print(f'Made it to checkpoint #5')
                                 print(f'Made it to checkpoint #5')

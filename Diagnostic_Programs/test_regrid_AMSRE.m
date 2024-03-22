b8aeba80d7f53b1a4ee100915188be4630ab14df
pcornillon <pcornillon@me.com>
2024-03-21 22:39:25 -0400

    Worked on code defining the average SST and the  latitude and longitude at center of each region averaged for SST.

--- Diagnostic_Programs/test_regrid_AMSRE.m
+++ Diagnostic_Programs/test_regrid_AMSRE.m
@@ -2,14 +2,18 @@
 
 % Test MODIS orbit. Get lon, lat and SST from this orbit.
 
-fi = '/Users/petercornillon/Data/temp_MODIS_L2_output_directory/output/SST/2011/01/AQUA_MODIS_orbit_046073_20110101T013634_L2_SST.nc4';
-
-MODIS_lon = ncread(fi, 'regridded_longitude');
-MODIS_lat = ncread(fi, 'regridded_latitude');
-MODIS_sst = ncread(fi, 'regridded_sst');
+if ~exist('MODIS_fi')
+    MODIS_fi = '~/Dropbox/Data/AMSR-R_regridding/AQUA_MODIS_orbit_046525_20110201T005330_L2_SST.nc4';
+    
+    MODIS_lon = ncread(MODIS_fi, 'regridded_longitude');
+    MODIS_lat = ncread(MODIS_fi, 'regridded_latitude');
+    MODIS_sst = ncread(MODIS_fi, 'regridded_sst');
+    
+    % AMSR_E_baseDir = '/Volumes/Aqua-1/AMSR-E_L2-v7/';
+    AMSR_E_baseDir = '/Users/petercornillon/Dropbox/Data/AMSR-R_regridding/';
+end
 
 % Test AMSR-E orbit.
 
-AMSR_E_baseDir = '/Volumes/Aqua-1/AMSR-E_L2-v7/';
+[outputArg1,outputArg2] = regrid_AMSRE( MODIS_fi, AMSR_E_baseDir, MODIS_lon, MODIS_lat, MODIS_sst)
 
-[outputArg1,outputArg2] = regrid_AMSRE( fi, AMSR_E_baseDir, MODIS_lon, MODIS_lat, MODIS_sst);
--- Functions/build_and_fix_orbits_Functions/regrid_AMSRE.m
+++ Functions/build_and_fix_orbits_Functions/regrid_AMSRE.m
@@ -39,12 +39,12 @@ function [outputArg1,outputArg2] = regrid_AMSRE( MODIS_fi, AMSR_E_baseDir, MODIS
 % corresponding AMSR-E orbit and we can check that the start times of the
 % AMSR-E and MODIS orbits are similar.
 
-if ~isempty(fi)
-    temp_time = ncread( fi, 'DateTime');
+if ~isempty(MODIS_fi)
+    temp_time = ncread( MODIS_fi, 'DateTime');
     matlab_time_MODIS_start = datenum([1970,1,1]) + double(temp_time)/86400;
 
-    kk = strfind( fi, '_orbit_');
-    NASA_orbit_t = str2num(fi(kk+7:kk+12));
+    kk = strfind( MODIS_fi, '_orbit_');
+    NASA_orbit_t = str2num(MODIS_fi(kk+7:kk+12));
 
     [MODIS_yr, MODIS_mn, MODIS_day, MODIS_hr, MODIS_min, MODIS_sec] = datevec(matlab_time_MODIS_start);
 else
@@ -61,31 +61,30 @@ year_s = return_a_string( 4, MODIS_yr);
 month_s = return_a_string( 2, MODIS_mn);
 day_s = return_a_string( 2, MODIS_day);
 
-fiamsre = [AMSR_E_baseDir year_s '/' year_s month_s day_s '-amsre-remss-l2p-l2b_v07_r' NASA_orbit '.dat-v01.nc'];
+AMSR_E_fi = [AMSR_E_baseDir year_s '/' year_s month_s day_s '-amsre-remss-l2p-l2b_v07_r' NASA_orbit(2:end) '.dat-v01.nc'];
 
 %       metadata_name
 %       data_name
 %       NASA_orbit_number
 
-datestr(datenum([1981,1,1]) + double(ref_time)/86400)
+% % % datestr(datenum([1981,1,1]) + double(ref_time)/86400)
 
-%%
+% AMSR_E_fi = '~/Dropbox/Data/AMSR-R_regridding/20110201-amsre-remss-l2p-l2b_v07_r46525.dat-v01.nc';
+% MODIS_fi = '~/Dropbox/Data/AMSR-R_regridding/AQUA_MODIS_orbit_046525_20110201T005330_L2_SST.nc4';
+% 
+% MODIS_lat = ncread( MODIS_fi, 'regridded_latitude');
+% MODIS_lon = ncread( MODIS_fi, 'regridded_longitude');
 
-fiamsre = '~/Dropbox/Data/AMSR-R_regridding/20110201-amsre-remss-l2p-l2b_v07_r46525.dat-v01.nc';
-MODIS_fi = '~/Dropbox/Data/AMSR-R_regridding/AQUA_MODIS_orbit_046525_20110201T005330_L2_SST.nc4';
+AMSR_E_lat = ncread( AMSR_E_fi, 'lat');
+AMSR_E_lon = ncread( AMSR_E_fi, 'lon');
 
-MODIS_lat = ncread( MODIS_fi, 'regridded_latitude');
-MODIS_lon = ncread( MODIS_fi, 'regridded_longitude');
+% % % MODIS_lonnadir = MODIS_lon(677,:);
+% % % MODIS_latnadir = MODIS_lat(677,:);
 
-AMSR_E_lat = ncread( fiamsre, 'lat');
-AMSR_E_lon = ncread( fiamsre, 'lon');
+% Get the closes equatorial crossing of the nadir track on the descending
+% portion of the orbit.
 
-MODIS_lonnadir = MODIS_lon(677,:);
-MODIS_latnadir = MODIS_lat(677,:);
-
-
-nn = find( abs(MODIS_latnadir) < 0.01);
-iEq = nn(3);
+iEq = find( min(abs(squeeze(MODIS_lat(677,20000:end)))) == abs(squeeze(MODIS_lat(677,20000:end)))) + 20000 - 1;
 
 % % % figure(1)
 % % % clf
@@ -97,6 +96,8 @@ iEq = nn(3);
 % % %     plot(MODIS_lon(:,nn(3)+i), MODIS_lat(:,nn(3)+i), '.b')
 % % % end
 
+% Build the L2eqa grid.
+
 pixsPerScan = size(MODIS_lon,1);
 
 scanMODIS_lon = MODIS_lon(1:677,iEq);
@@ -107,8 +108,8 @@ scanMODIS_lat = MODIS_lat(1:677,iEq);
 scanMODIS_lat(678) = mean(squeeze(MODIS_lat(677:678,iEq)));
 scanMODIS_lat(679:pixsPerScan+1) = MODIS_lat(678:pixsPerScan,iEq);
 
-centerLon = mean(scanMODIS_lon(677:678));
-centerLat = mean(scanMODIS_lat(677:678));
+% % % centerLon = mean(scanMODIS_lon(677:678));
+% % % centerLat = mean(scanMODIS_lat(677:678));
 
 lonSep = diff(scanMODIS_lon);
 latSep = diff(scanMODIS_lat);
@@ -124,6 +125,15 @@ for iDist=10:10:1162
     num2avgm(jDist) = length(nn);
     pixStartm(jDist) = nn(1);
     pixEndm(jDist) = nn(end);
+    
+    numPixElmnts = length(nn);
+    if rem(numPixElmnts,2)
+        pixSubStartm(jDist) = nn(ceil(numPixElmnts / 2));
+        pixSubEndm(jDist) = pixSubStartm(jDist);
+    else
+        pixSubStartm(jDist) = nn(numPixElmnts / 2);
+        pixSubEndm(jDist) = nn(numPixElmnts / 2) + 1;
+    end
 end
 
 for iDist=10:10:1162
@@ -133,24 +143,75 @@ for iDist=10:10:1162
     num2avgp(jDist) = length(nn);
     pixStartp(jDist) = nn(1) + pixsPerScan/2;
     pixEndp(jDist) = nn(end) + pixsPerScan/2;
+    
+    numPixElmnts = length(nn);
+    if rem(numPixElmnts,2)
+        pixSubStartp(jDist) = nn(ceil(numPixElmnts / 2));
+        pixSubEndp(jDist) = pixSubStartp(jDist);
+    else
+        pixSubStartp(jDist) = nn(numPixElmnts / 2);
+        pixSubEndp(jDist) = nn(numPixElmnts / 2) + 1;
+    end
 end
 
+% % longitude = MODIS_lon;
+% % threshold = 180;
+% % corrected_longitude = longitude;
+% % 
+% % % Detect and correct discontinuities for each scan line
+% % for i = 1:size(longitude, 2)
+% %     % Compute differences with wrapping at 360 degrees
+% %     differences = diff([longitude(1, i); longitude(:, i)]);  % Include the first element for proper diff calculation
+% %     jump_indices = find(abs(differences) > threshold);
+% %     
+% %     % Correct for each detected discontinuity
+% %     for j = jump_indices
+% %         if differences(j) > 0
+% %             % Forward jump detected, subtract 360 from subsequent points
+% %             corrected_longitude( j+1:end, i) = corrected_longitude( j+1:end, i) - 360;
+% %         else
+% %             % Backward jump detected, add 360 to subsequent points
+% %             corrected_longitude( j+1:end, i) = corrected_longitude( j+1:end, i) + 360;
+% %         end
+% %     end
+% % end
+
 %% Now average SST.
+
+lonThreshold = 180;
 
 numNewPixsm = length(pixStartm);
 for iPix=1:numNewPixsm
     jScanLine = 0;
-    for iScanLine=1:10:size(sst,2)-10
+    for iScanLine=1:10:size(MODIS_sst,2)-10
         jScanLine = jScanLine + 1;
         sstNew(numNewPixsm-iPix+1,jScanLine) = mean(MODIS_sst(pixStartm(iPix):pixEndm(iPix),iScanLine:iScanLine+10),'all','omitnan');
+        
+        L2eqaLat(numNewPixsm-iPix+1,jScanLine) = mean(MODIS_lat(pixSubStartm(iPix):pixSubEndm(iPix),iScanLine+4:iScanLine+5),'all','omitnan');
+        
+        lonElmnts = MODIS_lon(pixSubStartm(iPix):pixSubEndm(iPix),iScanLine+4:iScanLine+5);
+        if (max(lonElmnts(:)) - min(lonElmnts(:))) > lonThreshold
+                lonElmnts(lonElmnts<0) = lonElmnts(lonElmnts<0) + 360;
+        end
+        L2eqaLon(numNewPixsm-iPix+1,jScanLine) = mean(lonElmnts(:),'omitnan');
     end
 end
 
 numNewPixsp = length(pixStartp);
 for iPix=1:numNewPixsp
     jScanLine = 0;
-    for iScanLine=1:10:size(sst,2)-10
+    for iScanLine=1:10:size(MODIS_sst,2)-10
         jScanLine = jScanLine + 1;
-        sstNew(numNewPixsm + iPix,jScanLine) = mean(MODIS_sst(pixStartp(iPix):pixEndp(iPix),iScanLine:iScanLine+10),'all','omitnan');
+        sstNew(numNewPixsp + iPix,jScanLine) = mean(MODIS_sst(pixStartp(iPix):pixEndp(iPix),iScanLine:iScanLine+10),'all','omitnan');
+        
+        L2eqaLat(numNewPixsp + iPix,jScanLine) = mean(MODIS_lat(pixSubStartp(iPix):pixSubEndp(iPix),iScanLine+4:iScanLine+5),'all','omitnan');
+        
+        lonElmnts = MODIS_lon(pixSubStartp(iPix):pixSubEndp(iPix),iScanLine+4:iScanLine+5);
+        if (max(lonElmnts(:)) - min(lonElmnts(:))) > lonThreshold
+                lonElmnts(lonElmnts<0) = lonElmnts(lonElmnts<0) + 360;
+        end
+        L2eqaLon(numNewPixsp + iPix,jScanLine) = mean(lonElmnts(:),'omitnan');
     end
 end
+
+a = 180;

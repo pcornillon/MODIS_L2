f5d08aeaf7fc1bbe24e01314be0350b137098192
Peter Cornillon <pcornillon@me.com>
2024-04-12 09:21:48 -0400

    Added code to mount the uri disk at AWS

--- Shell_Scripts/spot_job.sh
+++ Shell_Scripts/spot_job.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Associate fixed IP address.
+
+su ubuntu -c 'aws --profile iam_pcornillon ec2 associate-address --allocation-id eipalloc-095c69c402b90902b --instance-id i-0da5f2a1336c0cd6f'
+
 # write commands to excecute here
 
-echo "Starting the script..."
+echo "" 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
+echo "" 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
+date 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
+echo "" 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
+echo "Starting the script..." 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
 
 touch /home/ubuntu/proof_of_life
 
@@ -16,17 +25,17 @@ OUTPUT_DIRECTORY="/mnt/uri-nfs-cornillon/Logs/nohup/"
 # Ensure the output directory exists, if it doesn't, create it.
 
 mkdir -p "$OUTPUT_DIRECTORY"
-echo "Checked for the output directory, created if it did not exist."
+echo "Checked for the output directory, created if it did not exist." 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
 
 # Make sure that we are using the most recent version of MODIS_L2
 
 cd "$MATLAB_PROJECT_DIRECTORY"
-echo "Pulling to $MATLAB_PROJECT_DIRECTORY"
-git pull
+echo "Pulling to $MATLAB_PROJECT_DIRECTORY" 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
+su ubuntu -c 'git pull' 
 
 # Sanity check to make sure that it pulled properly.
 
-sed -n '41p' "${MATLAB_PROJECT_DIRECTORY}batch_jobs/AWS_batch_test.m"
+sed -n '41p' "${MATLAB_PROJECT_DIRECTORY}batch_jobs/AWS_batch_test.m" 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
 
 # Start Matlab and run test script. The script it runs will exit after at least 75% (which could be changed, e.g./ to 100%) of the jobs have finished
 # or after the estimated required processing time has elapsed. It estimates this time based on the time for one of the submitted batch jobs to finish.
@@ -36,7 +45,7 @@ sed -n '41p' "${MATLAB_PROJECT_DIRECTORY}batch_jobs/AWS_batch_test.m"
 
 CURRENT_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 FILENAME="matlab_${CURRENT_TIME}.out"
-echo "Current time is $CURRENT_TIME and it will write the output for the Matlab portion to $FILENAME"
+echo "Current time is $CURRENT_TIME and it will write the output for the Matlab portion to $FILENAME" 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
 
 nohup matlab -nodisplay -nosplash -nodesktop -r "prj=openProject('${MATLAB_PROJECT_DIRECTORY}MODIS_L2.prj'); AWS_batch_test"  > "${OUTPUT_DIRECTORY}${FILENAME}" 2>&1 &
 

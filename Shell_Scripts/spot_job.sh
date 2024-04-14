4cad78c063f5d2b867bc23bc7cff97224965e719
Peter Cornillon <pcornillon@me.com>
2024-04-14 12:18:15 -0400

    Chat suggested that I run the matlab command with sudo... We'll see.

--- Shell_Scripts/spot_job.sh
+++ Shell_Scripts/spot_job.sh
@@ -65,7 +65,8 @@ echo "Current time is $CURRENT_TIME and it will write the output for the Matlab 
 
 echo "I am still $(whoami) and about to fire up Matlab." 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
 
-su ubuntu -c 'nohup matlab -nodisplay -nosplash -nodesktop -r "prj=openProject('${MATLAB_PROJECT_DIRECTORY}MODIS_L2.prj'); AWS_batch_test"  > "${OUTPUT_DIRECTORY}${FILENAME}" 2>&1 &'
+# su ubuntu -c 'nohup matlab -nodisplay -nosplash -nodesktop -r "prj=openProject('${MATLAB_PROJECT_DIRECTORY}MODIS_L2.prj'); AWS_batch_test"  > "${OUTPUT_DIRECTORY}${FILENAME}" 2>&1 &'
+sudo -u ubuntu -i bash -c 'nohup matlab -nodisplay -nosplash -nodesktop -r "prj=openProject('\''${MATLAB_PROJECT_DIRECTORY}MODIS_L2.prj'\''); AWS_batch_test" > "${OUTPUT_DIRECTORY}${FILENAME}" 2>&1 &'
 
 echo "I just started Matlab. Am still $(whoami). It should be running in the background." 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt
 echo -e "Here are the running Matlab jobs \n$(ps aux | grep MATLAB | grep -v grep)\n" 2>&1 | tee -a /mnt/uri-nfs-cornillon/session_log.txt

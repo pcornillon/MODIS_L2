c2b263276c99e72fd227fb2f5dc6ba15e57a4cb3
pcornillon <pcornillon@me.com>
2024-04-14 23:46:38 -0400

    Working with local tests. Need to try on my iMac, which is running a more recent version of Matlab.

--- Diagnostic_Programs/build_and_fix/AWS_batch_tester.m
+++ Diagnostic_Programs/build_and_fix/AWS_batch_tester.m
@@ -1,7 +1,8 @@
 % AWS_batch_tester - 
 %
 
-logs_directory = '/mnt/uri-nfs-cornillon/Logs/';
+% logs_directory = '/mnt/uri-nfs-cornillon/Logs/';
+logs_directory = '/Users/petercornillon/Desktop/Logs/';
 
 base_diary_filename = strrep(strrep([datestr(now) '_tester'], ':', 'h'), ' ', '_');
 
@@ -9,12 +10,12 @@ diary_filename = [logs_directory  base_diary_filename '.txt'];
 diary(diary_filename)
 
 % job_number(iJob) = batch( 'Tester', 0, {1}, CaptureDiary=true);
-Tester(1)
+tester(1)
 
 pause(60)
 
 % job_number(iJob) = batch( 'Tester', 0, {11}, CaptureDiary=true);
 
-Tester(11)
+tester(11)
 
 fprintf('To get status of these jobs use ''job_number(iJob).xxx'', where iJob is one of the job numbers above\n and xxx is a particular characteristic of the job such as State or RunningDuration.\n')
--- Shell_Scripts/tester_spot_job.sh
+++ Shell_Scripts/tester_spot_job.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
 
-# Associate fixed IP address, needed to mount uri disk but also makes things
-# easier since the requested spot instance will always have the same IP 
-# address: ubuntu@44.235.238.218
+# If LOCAL equals 0 then, associate fixed IP address, needed to mount uri disk 
+# but also makes things easier since the requested spot instance will always 
+# have the same IP  address: ubuntu@44.235.238.218. Will set directories for
+# AWS
+#
+# Otherwise will set directories for my laptop.
 
-MYID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+if [ $local -eq 0 ]
+then
+    OUTPUT_DIRECTORY="/mnt/uri-nfs-cornillon/"
+    MATLAB_DIRECTORY="/home/ubuntu/Documents/MODIS_L2/"
+    
+    # Commands to execute if the condition is true
+    MYID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
-su ubuntu -c "/usr/local/bin/aws --profile iam_pcornillon ec2 associate-address --allocation-id eipalloc-095c69c402b90902b --instance-id ${MYID}"
+    su ubuntu -c "/usr/local/bin/aws --profile iam_pcornillon ec2 associate-address --allocation-id eipalloc-095c69c402b90902b --instance-id ${MYID}"
 
-while true; do
-    myip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-    if [[ "$myip" == "44.235.238.218" ]]; then
-        break
-    else
-        sleep 2
-    fi
-done
+    while true; do
+        myip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+        if [[ "$myip" == "44.235.238.218" ]]; then
+            break
+        else
+            sleep 2
+        fi
+    done
 
-umount /mnt/uri-nfs-cornillon
-mount /mnt/uri-nfs-cornillon
+    umount /mnt/uri-nfs-cornillon
+    mount /mnt/uri-nfs-cornillon
+else
+    OUTPUT_DIRECTORY="/Users/petercornillon/Desktop/"
+    MATLAB_DIRECTORY="/Users/petercornillon/Git_repos/MODIS_L2/"    
+fi 
+
+OUTPUT_DIRECTORY_NOHUP="${OUTPUT_DIRECTORY}Logs/nohup/"
 
 # Start the session log.
 
-echo "" 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-echo "" 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-date 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-echo "" 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-echo "Starting the script..." 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
+echo "" 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "" 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+date 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "" 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "Starting the script..." 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
 
 # Generate the output filename for this job.s
 
 CURRENT_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 FILENAME="tester_${CURRENT_TIME}.out"
 
-echo "Current time is $CURRENT_TIME and it will write the output for the Matlab portion to $FILENAME" 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-echo "" 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-echo "Now you need to ssh to the spot instance: ssh -o StrictHostKeyChecking=no ubuntu@44.235.238.218" 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-echo "and submit Matlab job as follows: " 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-echo "" 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-echo "nohup matlab -nodisplay -nosplash -nodesktop -r \"prj=openProject('/home/ubuntu/Documents/MODIS_L2/MODIS_L2.prj'); AWS_batch_tester\" > \"/mnt/uri-nfs-cornillon/Logs/nohup/$FILENAME\" 2>&1 &" 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
-
-echo "Script execution completed." 2>&1 | tee -a /mnt/uri-nfs-cornillon/tester_session_log.txt
+echo "Current time is $CURRENT_TIME and it will write the output for the Matlab portion to $FILENAME" 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "" 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "Now you need to ssh to the spot instance: ssh -o StrictHostKeyChecking=no ubuntu@xx.xx.xx.xx" 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "and submit Matlab job as follows: " 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "" 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "nohup matlab -nodisplay -nosplash -nodesktop -r \"prj=openProject('${MATLAB_DIRECTORY}MODIS_L2.prj'), AWS_batch_tester, exit\" > \"${OUTPUT_DIRECTORY_NOHUP}$FILENAME\" 2>&1 &" 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "" 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt
+echo "Script execution completed." 2>&1 | tee -a ${OUTPUT_DIRECTORY}tester_session_log.txt

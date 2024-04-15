88fa1d006c5155674f294d83ceb0bdf29e00873b
pcornillon <pcornillon@me.com>
2024-04-14 21:41:28 -0400

    Changed Tester to non-batch.

--- Diagnostic_Programs/build_and_fix/AWS_batch_tester.m
+++ Diagnostic_Programs/build_and_fix/AWS_batch_tester.m
@@ -8,9 +8,13 @@ base_diary_filename = strrep(strrep([datestr(now) '_tester'], ':', 'h'), ' ', '_
 diary_filename = [logs_directory  base_diary_filename '.txt'];
 diary(diary_filename)
 
-job_number(iJob) = batch( 'Tester', 0, {1}, CaptureDiary=true);
+% job_number(iJob) = batch( 'Tester', 0, {1}, CaptureDiary=true);
+Tester(1)
+
 pause(60)
 
-job_number(iJob) = batch( 'Tester', 0, {11}, CaptureDiary=true);
+% job_number(iJob) = batch( 'Tester', 0, {11}, CaptureDiary=true);
+
+Tester(11)
 
 fprintf('To get status of these jobs use ''job_number(iJob).xxx'', where iJob is one of the job numbers above\n and xxx is a particular characteristic of the job such as State or RunningDuration.\n')

9891c920d268c067bc5eaae7afdd0925776c4021
Peter Cornillon <pcornillon@me.com>
2024-04-22 16:45:04 -0400

    Removed extra debug statements.

--- Diagnostic_Programs/build_and_fix/tester.m
+++ Diagnostic_Programs/build_and_fix/tester.m
@@ -11,7 +11,6 @@ end
 A2 = A^2;
 
 fprintf('You passed %f into this function. It''s squared value is: %f\n', A, A2)
-disp(['And with the disp command: You passed ' num2str(A) ' into this function. It''s squared value is: ' num2str(A2)])
 
 % Pause for 3 minutes
 
@@ -20,4 +19,3 @@ disp(['And with the disp command: You passed ' num2str(A) ' into this function. 
 A4 = A2^2;
 
 fprintf('And the input to the 4th power is:  %f\n', A4)
-disp(['And with the disp command: And the input to the 4th power is: ' num2str(A4)])

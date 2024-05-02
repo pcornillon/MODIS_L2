7de30498316a9a3bba1ff5724df4ca66f387bb50
Peter Cornillon <pcornillon@me.com>
2024-05-02 17:06:02 -0400

    Shortened the simple commands.

--- Diagnostic_Programs/simple_test.m
+++ Diagnostic_Programs/simple_test.m
@@ -1,9 +1,4 @@
 % simple_test.m -- just what it says - PCC
-
 prj=openProject('/Users/petercornillon/Git_repos/MODIS_L2/MODIS_L2.prj'); 
-
 x = 3
-
-x = x^3
-
 job_number(1) = batch( 'simple_batch', 0, {5});

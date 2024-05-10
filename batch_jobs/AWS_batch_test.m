7eef6178f0e5fea4437b1220ffabc3d7456df493
pcornillon <pcornillon@me.com>
2024-05-10 09:20:28 -0400

    New batch processing to start at [2016 12 1 0 0 0], each job to process 1 month, 20 new jobs.

--- batch_jobs/AWS_batch_test.m
+++ batch_jobs/AWS_batch_test.m
@@ -52,10 +52,10 @@ end
 % were to have entered [2002 7 1 0 0 0], the job would have started at
 % 00h00 on 30 June 2002.
 
-start_time = [2014 11 1 0 0 0];   % This is the start date/time the batch jobs are to use as [yyyy mm dd hh min ss]
+start_time = [2016 12 1 0 0 0];   % This is the start date/time the batch jobs are to use as [yyyy mm dd hh min ss]
 period_to_process = [0 1 0 4 0 0]; % This is the date/time range for each batch job entered as the number of [years months days hours minutes seconds]
 batch_step = [0 1 0 0 0 0]; % And the satellite date/time between the start of one batch job and the start of the next [yyyy mm dd hh min ss]
-num_batch = 25; % The number of batch jobs to submit
+num_batch = 20; % The number of batch jobs to submit
 
 % Define the time shift for the length of the interval to process, days,
 % hour, minutes and seconds; months will be handled in the loop.

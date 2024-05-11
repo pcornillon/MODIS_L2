a363242094574c86ce25e0e74969d22f6a94f5ef
pcornillon <pcornillon@me.com>
2024-05-11 14:30:26 -0400

    Added script to read and plot times to process orbits at AWS.

---
+++ Diagnostic_Programs/read_and_plot_processing_times.m
@@ -0,0 +1,59 @@
+% read_and_plot_processing_times - PCC
+%
+% Open and read the processing table file written at AWS by reading the
+% build_and_fix_orbit log files. This file contains a number for the batch
+% job that processed the file, a number for the orbit processed, the
+% processing time in seconds and the time since 1970,1,1 that the
+% processing of the orbit was completed. Then plot the times to process
+% versus the time processing was completed.
+%
+% The script to generate the table at AWS is MODIS_L2/Shell_scripts/generate processing_times_table.py 
+% To copy from AWS: scp ubuntu@ec2-52-11-152-158.us-west-2.compute.amazonaws.com:/mnt/uri-nfs-cornillon/Logs/processing_times.txt /Users/petercornillon/Dropbox/Data/From_AWS/
+%
+
+% Specify the file name
+filename = '/Users/petercornillon/Dropbox/Data/From_AWS/processing_times.txt';
+
+% Open the file for reading
+fileID = fopen(filename, 'r');
+
+% Define the format for each line (4 integers/floats per line)
+formatSpec = '%d %d %d %f';
+
+% Read the data from the file using textscan
+data = textscan(fileID, formatSpec);
+
+% Close the file
+fclose(fileID);
+
+% Extract columns into separate variables (if needed)
+iBatchJob = data{1}; % batch job index
+iOrbit = data{2}; % orbit processed in this batch job
+LinuxTime = data{3}; % time of processing in seconds since 1/1/1970
+processingTime = data{4}; % time to process
+
+% Alternatively, you can store the data in a matrix form like this:
+resultMatrix = [iBatchJob, iOrbit, LinuxTime, processingTime];
+
+% Convert seconds since 1/1/1970 to Matlab time. Subtract 4 hours for time
+% zone difference (I think)
+
+matTime = datetime( double(LinuxTime), 'ConvertFrom', 'posixtime') - 4 / 24;
+
+% Now plot the data
+
+figure(1)
+plot( matTime, processingTime, '.')
+set(gca, fontsize=20)
+grid on
+
+xlabel('Time');
+ylabel('Values');
+title('Processing Time vs Time Processed');
+
+% Optionally, customize the date format on the x-axis
+ax = gca;
+ax.XAxis.TickLabelFormat = 'yyyy-MM-dd HH:mm:ss';
+
+% Improve the layout
+datetick('x', 'yyyy-mm-dd HH:MM:SS', 'keepticks');

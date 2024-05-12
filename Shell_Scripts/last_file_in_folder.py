6b169881a65d142ba8352d9e0f3560b28f7163d9
pcornillon <pcornillon@me.com>
2024-05-12 18:34:54 -0400

    Added file to list the last file in all folders and subfolders in a given one.

---
+++ Shell_Scripts/last_file_in_folder.py
@@ -0,0 +1,28 @@
+import os
+
+def get_last_file_in_folders(base_path, year):
+    last_files = {}
+    full_path = os.path.join(base_path, str(year))
+    
+    # Traverse the directory structure
+    for root, dirs, files in os.walk(full_path):
+        if files:
+            # Sort files by modification time
+            files.sort(key=lambda f: os.path.getmtime(os.path.join(root, f)))
+            # Get the last file
+            last_file = files[-1]
+            last_files[root] = last_file
+    return last_files
+
+# Base path to the folders
+base_path = '/mnt/uri-nfs-cornillon/SST/'
+
+# Year to be passed in
+year = 2011
+
+# Get the last file in each folder for the specified year
+last_files = get_last_file_in_folders(base_path, year)
+
+# Print the results
+for folder, file in last_files.items():
+    print(f"Folder: {folder}, Last file: {file}")

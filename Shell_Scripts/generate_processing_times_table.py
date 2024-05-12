6dfbddccabbd4caa823774ff8fa866581a8cad9a
pcornillon <pcornillon@me.com>
2024-05-12 12:47:23 -0400

    Version with older print statements.

--- Shell_Scripts/generate_processing_times_table.py
+++ Shell_Scripts/generate_processing_times_table.py
@@ -1,65 +1,53 @@
-import re
-import os
 import argparse
-from datetime import datetime
-import calendar
+import os
 import pytz
+from datetime import datetime
+from collections import defaultdict
 
-def extract_processing_times(files_list, data_output_filename='processing_times.txt', index_output_filename='file_index.txt'):
-    time_pattern = re.compile(r'Time to process and save .*\.nc4:\s*([\d.]+)\s*seconds\. Current date/time:\s*(\d{2}-\w{3}-\d{4} \d{2}:\d{2}:\d{2})')
-    local_tz = pytz.timezone('America/New_York')
-    processing_data = []
-    file_index = []
-    file_number = 0
+def extract_processing_times(directory, output_file, index_file):
+    logs_data = defaultdict(list)
+    log_files = [f for f in os.listdir(directory) if f.endswith(".txt") and "May" in f]
+    log_files.sort()
+    
+    file_index = {}
+    for idx, file in enumerate(log_files, start=1):
+        file_index[file] = idx
+        orbit_num = 0
 
-    for file_path in files_list:
-        file_number += 1
-        filename = os.path.basename(file_path)
-        file_index.append((file_number, filename))
-        orbit_number = 0
-        line_count = 0
+        with open(os.path.join(directory, file), "r") as log:
+            for line in log:
+                if "Time to process and save" in line:
+                    orbit_num += 1
 
-        print("Processing file {}: {}".format(file_number, filename))
-        
-        with open(file_path, 'r') as file:
-            for line in file:
-                line_count += 1
-                orbit_match = re.match(r'Working on orbit #(\d+)', line)
-                if orbit_match:
-                    orbit_number = int(orbit_match.group(1))
-                
-                time_match = time_pattern.search(line)
-                if time_match:
-                    processing_time = float(time_match.group(1))
-                    date_time_str = time_match.group(2)
+                    # Extract the processing time and original date/time
+                    processing_time_str = line.split(":")[-2].strip()
+                    original_time_str = line.split("Current date/time:")[-1].strip()
+                    
+                    # Parse the date/time string and convert to Unix timestamp
+                    dt = datetime.strptime(original_time_str, "%d-%b-%Y %H:%M:%S")
+                    dt_utc = dt.replace(tzinfo=pytz.timezone("UTC"))
+                    unix_timestamp = int((dt_utc - datetime(1970, 1, 1, tzinfo=pytz.UTC)).total_seconds())
 
-                    dt = datetime.strptime(date_time_str, '%d-%b-%Y %H:%M:%S')
-                    dt_local = local_tz.localize(dt)
-                    dt_utc = dt_local.astimezone(pytz.utc)
-                    unix_timestamp = calendar.timegm(dt_utc.timetuple())
+                    # Append the results to the logs_data list
+                    logs_data[idx].append((idx, orbit_num, unix_timestamp, float(processing_time_str)))
 
-                    print("Original time: {}, Local time: {}, UTC time: {}, Unix timestamp: {}".format(date_time_str, dt_local, dt_utc, unix_timestamp))
+    # Write results to output file
+    with open(output_file, "w") as out:
+        for file_idx, data_list in logs_data.items():
+            for data in data_list:
+                out.write("%d %d %d %.1f\n" % (data[0], data[1], data[2], data[3]))
 
-                    processing_data.append((file_number, orbit_number, unix_timestamp, processing_time))
-            
-            print("Processed {} lines in file {}: {}".format(line_count, file_number, filename))
+    # Write file index to index file
+    with open(index_file, "w") as index_out:
+        for file_name, idx in file_index.items():
+            index_out.write("%d %s\n" % (idx, file_name))
 
-    with open(data_output_filename, 'w') as data_output_file:
-        for data in processing_data:
-            data_output_file.write('{} {} {} {}\n'.format(*data))
-
-    with open(index_output_filename, 'w') as index_output_file:
-        for index in file_index:
-            index_output_file.write('{} {}\n'.format(*index))
-
-    print("Processing times extracted and saved to:", data_output_filename)
-    print("File index saved to:", index_output_filename)
-
+# Argument parsing
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Extract processing times and timestamps from multiple log files.')
-    parser.add_argument('-f', '--files', nargs='+', required=True, help='List of log files to process')
-    parser.add_argument('-o', '--data_output', default='processing_times.txt', help='Output file name to save processing times (default: processing_times.txt)')
-    parser.add_argument('-i', '--index_output', default='file_index.txt', help='Output file name to save file indices (default: file_index.txt)')
+    parser = argparse.ArgumentParser(description="Extract processing times from log files.")
+    parser.add_argument("-d", "--directory", required=True, help="Directory containing log files")
+    parser.add_argument("-o", "--data_output", required=True, help="Output file for processing times data")
+    parser.add_argument("-i", "--index_output", required=True, help="Output file for file index")
     args = parser.parse_args()
 
-    extract_processing_times(args.files, args.data_output, args.index_output)
+    extract_processing_times(args.directory, args.data_output, args.index_output)

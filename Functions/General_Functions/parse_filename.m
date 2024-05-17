5a51442d4cc3a1d447c835361dcab3f56662cc1d
pcornillon <pcornillon@me.com>
2024-05-16 22:32:26 -0400

    Added script to extract the date and time from a MODIS granule filename and return it as Matlab time.

---
+++ Functions/General_Functions/parse_filename.m
@@ -0,0 +1,36 @@
+function matTime = parse_filename( filename)
+% parse_filename - returns Matlab time given a NASA MODIS filename - PCC
+%
+% INPUT
+%   filename - a NASA filename of the form AQUA_MODIS.20240101T000001.L2.SST.nc
+%
+% OUTPUT
+%   matTime - days since 1/1/1 extracted from the filename
+%
+
+nn = strfind(filename, 'MODIS.') + 6;
+mm = nn + 3;
+
+Year = str2num(filename(nn:mm));
+nn = mm + 1;
+mm = nn + 1;
+
+Month = str2num(filename(nn:mm));
+nn = mm + 1;
+mm = nn + 1;
+
+Day = str2num(filename(nn:mm));
+nn = mm + 2;
+mm = nn + 1;
+
+Hour = str2num(filename(nn:mm));
+nn = mm + 1;
+mm = nn + 1;
+
+Minute = str2num(filename(nn:mm));
+nn = mm + 1;
+mm = nn + 1;
+
+Second = str2num(filename(nn:mm));
+
+matTime = datenum([Year, Month, Day, Hour, Minute, Second]);

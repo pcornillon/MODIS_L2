303fb92031c67b5f72b463227cc658ec48a6294a
Peter Cornillon <pcornillon@me.com>
2024-05-28 23:14:20 -0400

    Updated script generating the list of metadata files to read the attributes from each metadata file and to add that to the list.

--- Main_Programs/get_granule_filelists.m
+++ Main_Programs/get_granule_filelists.m
@@ -4,6 +4,8 @@ function get_granule_filelists(Years)
 % INPUT
 %   Years: vector of years to process [2002:2005]
 
+metadata_directory = '/Volumes/MODIS_L2_modified/OBPG/Data_from_OBPG_for_PO-DAAC/';
+
 Test = 0;
 
 for iYear=1:length(Years)
@@ -20,20 +22,23 @@ for iYear=1:length(Years)
         % load ~/Dropbox/TempForTransfer/filelist.mat
         load ~/Desktop/filelist.mat
     else
-        eval(['fileList = dir(''/Volumes/MODIS_L2_Modified/OBPG/Data_from_OBPG_for_PO-DAAC/' YearString '/AQUA*'');'])
+        eval(['fileList = dir([metadata_directory ''' YearString '/AQUA*'');'])
     end
     
     granuleList(length(fileList)).filename = fileList(end).name;
-    granuleList(length(fileList)).matTime = parse_filename(fileList(end).name);
+    granuleList(length(fileList)).filename_time = parse_filename(fileList(end).name);
     
     for iFile=1:length(fileList)
         filename = fileList(iFile).name;
-        
+
         granuleList(iFile).filename = filename;
-        granuleList(iFile).matTime = parse_filename(filename);
+        granuleList(iFile).filename_time = parse_filename(filename);
+
+        tempTime = ncreadatt( [metadata_directory num2str(year(granuleList(iGranule).filename_time)) '/' granuleList(iGranule).filename], '/', 'time_coverage_start');
+        granuleList(iFile).granule_start_time = datenum(datetime(tempTime, 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss.SSS''Z''', 'TimeZone', 'UTC'));
     end
     
-    eval(['save(''~/Dropbox/Data/MODIS_L2/granuleList_' YearString '.mat'', ''granuleList'');']);
+    eval(['save(''~/Dropbox/Data/MODIS_L2/NewGranuleList_' YearString '.mat'', ''granuleList'');']);
     
     clear granuleList fileList
     

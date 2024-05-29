9ecc2ef24fce6ffcab6e2c246bac17796cf6cc76
Peter Cornillon <pcornillon@me.com>
2024-05-29 08:57:12 -0400

    Beginning of script to compare different version--an older one in which I am confident and a newer one after many changes-- of the output for a given orbit.

---
+++ Diagnostic_Programs/build_and_fix/compare_orbits.m
@@ -0,0 +1,8 @@
+function compare_orbits( fiOne, fiTwo)
+% compare_orbits - reads in all fields in two orbits and differences them - PCC
+%
+% INPUT
+%   fiOne: complete filename for the first orbit.
+%   fiTwo: complete filename for the second orbit.
+
+% Get all the fields to read.

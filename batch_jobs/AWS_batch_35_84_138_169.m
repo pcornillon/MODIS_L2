f24006aa6ff4c61986ac7f8545c75a22c456a3aa
Peter Cornillon <pcornillon@me.com>
2024-08-11 19:22:04 -0400

    Updated spot, specification and ASWS_batch script for 35... and 52... to process with x2iedn.16xlarge

--- batch_jobs/AWS_batch_35_84_138_169.m
+++ batch_jobs/AWS_batch_35_84_138_169.m
@@ -22,10 +22,13 @@
 %           trip in France. 
 %   2.0.1 - modified to process from 7/1/2007 in steps of 1 month,
 %           submitting a new job as soon as one ends. End time 6/30/2012.
+%   2.0.2 - 8/11/2024 - Modified to switch to x2iedn.24xlarge processing.
+%           Starting from 1/1/2009. Submitting 72 jobs to process 6 years.
+%           - PCC
 
 global version_struct
 
-version_struct.AWS_batch_35_84_138_169 = '2.0.1';
+version_struct.AWS_batch_35_84_138_169 = '2.0.2';
 
 % There is a test mode, which, if set to 1, allows you to run this script
 % without submitting any jobs. It will however print out the range of dates
@@ -57,10 +60,10 @@ end
 % were to have entered [2002 7 1 0 0 0], the job would have started at
 % 00h00 on 30 June 2002.
 
-start_time = [2007 7 1 0 0 0];   % This is the start date/time the batch jobs are to use as [yyyy mm dd hh min ss]
+start_time = [2009 1 1 0 0 0];   % This is the start date/time the batch jobs are to use as [yyyy mm dd hh min ss]
 period_to_process = [0 1 0 4 0 0]; % This is the date/time range for each batch job entered as the number of [years months days hours minutes seconds]
 batch_step = [0 1 0 0 0 0]; % And the satellite date/time between the start of one batch job and the start of the next [yyyy mm dd hh min ss]
-num_batch = 60; % The number of batch jobs to submit
+num_batch = 72; % The number of batch jobs to submit
 
 % Define the time shift for the length of the interval to process, days,
 % hour, minutes and seconds; months will be handled in the loop.

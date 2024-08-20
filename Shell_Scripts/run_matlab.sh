351a5cb17f789a2b12d0db3dae90b26e33cbf79c
pcornillon <pcornillon@me.com>
2024-08-20 12:57:32 -0400

    Added output lines and fixed some filename issues.

--- Shell_Scripts/run_matlab.sh
+++ Shell_Scripts/run_matlab.sh
@@ -9,9 +9,17 @@ BATCH_JOB_FILENAME = $1
 LOCAL_CURRENT_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 
 AWS_FILENAME="AWS_copy_${LOCAL_CURRENT_TIME}.out"
-MATLAB_FILENAME="matlab_$(date +'%Y-%m-%d_%H-%M-%S').txt"
 LOCAL_SESSION_FILENAME="local_session_${LOCAL_CURRENT_TIME}.txt"
 REMOTE_SESSION_FILENAME="remote_session_${LOCAL_CURRENT_TIME}.txt"
+MATLAB_FILENAME="matlab_${LOCAL_CURRENT_TIME}.txt"
+
+echo "" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
+echo "Will write to the following files:" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
+echo "AWS_FILENAME: ${LOCAL_OUTPUT_DIRECTORY}/$AWS_FILENAME" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$AWS_FILENAME"
+echo "LOCAL_SESSION_FILENAME: ${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
+echo "REMOTE_SESSION_FILENAME: ${LOCAL_OUTPUT_DIRECTORY}/$REMOTE_SESSION_FILENAME" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$REMOTE_SESSION_FILENAME"
+echo "MATLAB_FILENAME: ${LOCAL_OUTPUT_DIRECTORY}/$MATLAB_FILENAME" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$MATLAB_FILENAME"
+echo "" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
 
 # Define the output directory for the log file for this session.
 
@@ -25,16 +33,16 @@ touch /home/ubuntu/proof_of_life
 # Ensure the output directory exists, if it doesn't, create it.
 
 mkdir -p "$LOCAL_OUTPUT_DIRECTORY"
-echo "Checked for the output directory, created if it did not exist." | tee -a "${LOCAL_OUTPUT_DIRECTORY}//$AWS_FIL$LOCAL_SESSION_FILENAME"
+echo "Checked for the output directory, created if it did not exist." | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
 
 # Some output.
 
-echo "" | tee -a "${LOCAL_OUTPUT_DIRECTORY}//$AWS_FIL$LOCAL_SESSION_FILENAME"
-echo "" | tee -a "${LOCAL_OUTPUT_DIRECTORY}//$AWS_FIL$LOCAL_SESSION_FILENAME"
-date  | tee -a "${LOCAL_OUTPUT_DIRECTORY}//$AWS_FIL$LOCAL_SESSION_FILENAME"
-echo "" | tee -a "${LOCAL_OUTPUT_DIRECTORY}//$AWS_FIL$LOCAL_SESSION_FILENAME"
-echo "Starting the script" | tee -a "${LOCAL_OUTPUT_DIRECTORY}//$AWS_FIL$LOCAL_SESSION_FILENAME"
-echo "I am $(whoami) and proud of it" | tee -a "${LOCAL_OUTPUT_DIRECTORY}//$AWS_FIL$LOCAL_SESSION_FILENAME"
+echo "" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
+echo "" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
+date  | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
+echo "" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
+echo "Starting the script" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
+echo "I am $(whoami) and proud of it" | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
 
 # Change to the git repo directory for this project and pull the latest changes as user ubuntu
 
@@ -51,7 +59,7 @@ nohup python "${LOCAL_MATLAB_PROJECT_DIRECTORY}Shell_Scripts/AWS_copy_nc4_to_rem
 
 # Start Matlab and submit the jobs to submit batch jobs for processing. 
 
-echo "I am about to fire up Matlab." 2>&1 | tee -a "${LOCAL_OUTPUT_DIRECTORY}//$AWS_FIL$LOCAL_SESSION_FILENAME"
+echo "I am about to fire up Matlab." 2>&1 | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
 
 sudo -u ubuntu bash -c '
   export REMOTE_OUTPUT_DIRECTORY="/mnt/uri-nfs-cornillon/Logs/"
@@ -65,5 +73,5 @@ sudo -u ubuntu bash -c '
   nohup matlab -batch "prj=openProject('\''$REMOTE_MATLAB_PROJECT_DIRECTORY/MODIS_L2.prj'\''); $BATCH_JOB_FILENAME;" > "$REMOTE_OUTPUT_DIRECTORY/$MATLAB_FILENAME" 2>&1 &
   echo "Just started Matlab."  | tee -a "${REMOTE_OUTPUT_DIRECTORY}/$REMOTE_SESSION_FILENAME"
 
-echo "I just started Matlab. Am still $(whoami). It should be running in the background. This script is finished." | tee -a "${LOCAL_OUTPUT_DIRECTORY}//$AWS_FIL$LOCAL_SESSION_FILENAME"
+echo "I just started Matlab. Am still $(whoami). It should be running in the background. This script is finished." | tee -a "${LOCAL_OUTPUT_DIRECTORY}/$LOCAL_SESSION_FILENAME"
 

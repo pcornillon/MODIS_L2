277a11fa404719cf7686533d1800896be8eb8854
pcornillon <pcornillon@me.com>
2024-08-21 11:43:45 -0400

    Cleaned up this scrip.

--- Shell_Scripts/run_matlab.sh
+++ Shell_Scripts/run_matlab.sh
@@ -9,16 +9,9 @@ BATCH_JOB_FILENAME="$1"
 LOCAL_CURRENT_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 
 AWS_FILENAME="AWS_copy_${LOCAL_CURRENT_TIME}.out"
-echo "AWS_FILENAME: $AWS_FILENAME"
-
 LOCAL_SESSION_FILENAME="local_session_$LOCAL_CURRENT_TIME.txt"
-echo "LOCAL_SESSION_FILENAME: $LOCAL_SESSION_FILENAME"
-
 REMOTE_SESSION_FILENAME="remote_session_${LOCAL_CURRENT_TIME}.txt"
-echo "REMOTE_SESSION_FILENAME: $REMOTE_SESSION_FILENAME"
-
 MATLAB_FILENAME="matlab_${LOCAL_CURRENT_TIME}.txt"
-echo "MATLAB_FILENAME: $MATLAB_FILENAME"
 
 # Define the output directory for the log file for this session.
 
@@ -26,12 +19,6 @@ LOCAL_OUTPUT_DIRECTORY="/mnt/uri-nfs-cornillon/Logs/"
 LOCAL_MATLAB_PROJECT_DIRECTORY="/home/ubuntu/Documents/MODIS_L2/"
 
 LOCAL_OUTPUT_DIRECTORY_NOHUP="/mnt/uri-nfs-cornillon/Logs/nohup/"
-
-# Write out the names of output log files.
-
-echo ${LOCAL_OUTPUT_DIRECTORY}
-echo $LOCAL_SESSION_FILENAME
-echo ${LOCAL_OUTPUT_DIRECTORY}$LOCAL_SESSION_FILENAME
 
 echo "" | tee -a "${LOCAL_OUTPUT_DIRECTORY}$LOCAL_SESSION_FILENAME"
 echo "Will write to the following files:" | tee -a "${LOCAL_OUTPUT_DIRECTORY}$LOCAL_SESSION_FILENAME"
@@ -72,38 +59,51 @@ echo "Current time is $LOCAL_CURRENT_TIME and it will write the output for the P
 
 nohup python "${LOCAL_MATLAB_PROJECT_DIRECTORY}Shell_Scripts/AWS_copy_nc4_to_remote.py" > "${LOCAL_OUTPUT_DIRECTORY}${AWS_FILENAME}" 2>&1 &
 
-# Start Matlab and submit the jobs to submit batch jobs for processing. 
+$ Write local variables, which will be used and also displayed in the sudo command.
 
 echo $LOCAL_OUTPUT_DIRECTORY
 echo $LOCAL_SESSION_FILENAME
-echo "I am about to fire up Matlab." 2>&1 | tee -a "${LOCAL_OUTPUT_DIRECTORY}$LOCAL_SESSION_FILENAME"
-
 echo $REMOTE_SESSION_FILENAME
 
-sudo -E -u ubuntu bash -c '
+# Start Matlab and submit the jobs to submit batch jobs for processing. 
+
+echo "I am about to fire up Matlab." 2>&1 | tee -a "${LOCAL_OUTPUT_DIRECTORY}$LOCAL_SESSION_FILENAME"
+
+sudo -u ubuntu bash -c '
+
+  # Create new variables unique to this command.
+
   export REMOTE_OUTPUT_DIRECTORY="/mnt/uri-nfs-cornillon/Logs/"
   export REMOTE_MATLAB_PROJECT_DIRECTORY="/home/ubuntu/Documents/MODIS_L2/"
   export REMOTE_OUTPUT_DIRECTORY_NOHUP="/mnt/uri-nfs-cornillon/Logs/nohup/"
+
+  # Reassign variables from outside of this command.
 
   REMOTE_SESSION_FILENAME='$REMOTE_SESSION_FILENAME'
   LOCAL_SESSION_FILENAME='$LOCAL_SESSION_FILENAME'
   MATLAB_FILENAME='$MATLAB_FILENAME'
   BATCH_JOB_FILENAME='$BATCH_JOB_FILENAME'
 
+  # Make sure that they are correct.
+
   echo $REMOTE_OUTPUT_DIRECTORY
   echo $REMOTE_SESSION_FILENAME
   echo $BATCH_JOB_FILENAME
 
   echo "Am running in sudo submitted version of script." | tee -a "${REMOTE_OUTPUT_DIRECTORY}$REMOTE_SESSION_FILENAME"
-  cd "$REMOTE_MATLAB_PROJECT_DIRECTORY"
+  
+  # cd to the git directory and pull the updates.
 
+  cd "$REMOTE_MATLAB_PROJECT_DIRECTORY"
   echo "Pulling to $REMOTE_MATLAB_PROJECT_DIRECTORY as user $(whoami)" | tee -a "${REMOTE_OUTPUT_DIRECTORY}$REMOTE_SESSION_FILENAME"
   git pull
+
   echo "Starting Matlab as user $(whoami)" | tee -a "${REMOTE_OUTPUT_DIRECTORY}$REMOTE_SESSION_FILENAME"
 
   echo 'nohup matlab -batch "prj=openProject('\"$REMOTE_MATLAB_PROJECT_DIRECTORY/MODIS_L2.prj\"'); run('\"$BATCH_JOB_FILENAME\"');" > \"$REMOTE_OUTPUT_DIRECTORY$MATLAB_FILENAME\" 2>&1 &'
   # echo 'nohup matlab -batch "prj=openProject('\''$REMOTE_MATLAB_PROJECT_DIRECTORY/MODIS_L2.prj'\''); run(\''$BATCH_JOB_FILENAME\'');" > "$REMOTE_OUTPUT_DIRECTORY$MATLAB_FILENAME" 2>&1 &'
   # nohup matlab -batch "prj=openProject('\''$REMOTE_MATLAB_PROJECT_DIRECTORY/MODIS_L2.prj'\''); run(\''$BATCH_JOB_FILENAME\'');" > "$REMOTE_OUTPUT_DIRECTORY$MATLAB_FILENAME" 2>&1 &
+  
   nohup matlab -batch "prj=openProject('\"$REMOTE_MATLAB_PROJECT_DIRECTORY/MODIS_L2.prj\"'); run('\"$BATCH_JOB_FILENAME\"');" > \"$REMOTE_OUTPUT_DIRECTORY$MATLAB_FILENAME\" 2>&1 &
   echo "Just started Matlab."  | tee -a "${REMOTE_OUTPUT_DIRECTORY}$REMOTE_SESSION_FILENAME" '
 

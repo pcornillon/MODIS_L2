6f10c99ac6b7c8c9b7d090f1233642c05d3270c4
pcornillon <pcornillon@me.com>
2024-08-26 12:31:17 -0400

    Undid the debug statement I had added, which isn't necessary after all and broke the script anyway.

--- Shell_Scripts/AWS_copy_nc4_to_remote.py
+++ Shell_Scripts/AWS_copy_nc4_to_remote.py
@@ -10,8 +10,6 @@ from datetime import datetime
 base_input_folder = '/datadisk/SST/'
 base_output_folder = '/mnt/uri-nfs-cornillon/'
 
-print( "Just checking: base_input_folder" % Just checking to see if anything in output.
-
 class DualLogger:
     def __init__(self, log_file_path):
         self.terminal = sys.stdout

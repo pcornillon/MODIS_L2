227ae4e2c3cee73dc8055606eed4b3a0ec75629f
pcornillon <pcornillon@me.com>
2024-08-25 21:20:51 -0400

    Modified to copy to /Volumes/MODIS_L2_Modify/OBPG/SST_Orbits/

--- Shell_Scripts/sync_datadisk_to_local.sh
+++ Shell_Scripts/sync_datadisk_to_local.sh
@@ -3,8 +3,8 @@
 # Variables
 REMOTE_USER="ubuntu"
 REMOTE_HOST="ec2-52-11-152-158.us-west-2.compute.amazonaws.com"
-REMOTE_BASE_DIR="/datadisk/SST"
-LOCAL_BASE_DIR="."
+REMOTE_BASE_DIR="/datadisk/SST/"
+LOCAL_BASE_DIR="/Volumes/MODIS_L2_Modified/OBPG/SST_Orbits/"
 SSH_KEY="~/.ssh/id_rsa"  # Path to your SSH private key
 
 # Specify the year you want to copy files for
@@ -14,7 +14,7 @@ YEAR=$1
 REMOTE_DIR="${REMOTE_BASE_DIR}/${YEAR}"
 
 # Use rsync to recursively copy all .nc4 files from the remote directory to the local directory
-rsync -avz --include="*/" --include="*.nc4" --exclude="*" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/" "${LOCAL_BASE_DIR}/"
+rsync -avz --include="*/" --include="*.nc4" --exclude="*" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/" "${LOCAL_BASE_DIR}${YEAR}"
 
 # Check if the rsync command was successful
 if [ $? -eq 0 ]; then

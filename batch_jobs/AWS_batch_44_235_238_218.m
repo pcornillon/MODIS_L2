95f06c57dbe2126dc20feea9bcf1f985601224f8
pcornillon <pcornillon@me.com>
2024-08-27 22:25:21 -0400

    Added new AWS_batch scripts.

--- batch_jobs/AWS_batch_44_235_238_218.m
+++ batch_jobs/AWS_batch_44_235_238_218.m
@@ -35,10 +35,15 @@
 %           December 2008. Am switching to x2iedn.16xlarge. This computer
 %           has 64 virtural cores and 1.9 TB of main memory, so Matlab will
 %           default to 32 jobs at once - PCC
+%   2.0.5 - 8/27/2024 - modified to process from 03/01/2005 in steps of 1
+%           month, but submitting 58 jobs to take the processing through
+%           December 2009. Am switching to x2idn.16xlarge. This computer
+%           has 64 virtural cores and 1.9 TB of main memory, so Matlab will
+%           default to 32 jobs at once - PCC
 
 global version_struct
 
-version_struct.AWS_batch_44_235_238_218 = '2.0.4';
+version_struct.AWS_batch_44_235_238_218 = '2.0.5';
 
 % There is a test mode, which, if set to 1, allows you to run this script
 % without submitting any jobs. It will however print out the range of dates
@@ -70,10 +75,10 @@ end
 % to have entered [2002 7 0 0 0 0], the job would have started at 00h00 on
 % 30 June 2002. 
 
-start_time = [2002 12 01 0 0 0];   % This is the start date/time the batch jobs are to use as [yyyy mm dd hh min ss]
+start_time = [2005 3 01 0 0 0];   % This is the start date/time the batch jobs are to use as [yyyy mm dd hh min ss]
 period_to_process = [0 1 0 4 0 0]; % This is the date/time range for each batch job entered as the number of [years months days hours minutes seconds]
 batch_step = [0 1 0 0 0 0]; % And the satellite date/time between the start of one batch job and the start of the next [yyyy mm dd hh min ss]
-num_batch = 73; % The number of batch jobs to submit
+num_batch = 58; % The number of batch jobs to submit
 
 % Define the time shift for the length of the interval to process, days,
 % hour, minutes and seconds; months will be handled in the loop.

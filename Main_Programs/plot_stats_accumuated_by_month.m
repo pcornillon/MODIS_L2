f82172040f309b130739b03f3b6b7f96846e1d2b
pcornillon <pcornillon@me.com>
2024-09-25 06:41:01 -0700

    Added some more plots

--- Main_Programs/plot_stats_accumuated_by_month.m
+++ Main_Programs/plot_stats_accumuated_by_month.m
@@ -1,7 +1,21 @@
 function gradStructure = plot_stats_accumuated_by_month( figNo, yearsToProcess, temporalIntervals)
 %
+% Usage: To plot a number of years use the following. 
+%
+% This will run the plotting function three times, plotting 4 figures. The
+% figure number will be the year - 2000 with 1 to 4 appended to the end. So
+% for 2008 they will be figures 81, 82, 83, 84 and for 2018, 181. 182, 183,
+% 184
+%
+% for iYear=[2008 2011 2018]
+%     yearS = num2str(iYear);
+%     year2dS = num2str(iYear - 2000);
+%     eval(['gradStructure = plot_stats_accumuated_by_month( ' year2dS '1, [' yearS '], ''Years'');'])
+% end
+%
+% You can, of course just run it on its own. To do plots for 2008 through 2010
+% starting with figure #1: gradStructure = plot_stats_accumuated_by_month( 1, [2008:2010], 'Years')
 
-figNo = figNo - 1;
 axisFontSize = 18;
 titleFontSize = 30;
 
@@ -15,6 +29,24 @@ colormap(cmap);
 set(gca, 'Color', [0.7 0.7 0.7]); % Gray background for NaN values
 
 load coastlines; % MATLAB built-in coastline data
+
+% Meander peak locations found in Agulhas in 2008
+
+mpx = [28.5183
+   34.1728
+   42.0890
+   48.1204
+   52.8325
+   57.7330
+   62.2565];
+
+mpy = [-36.8863
+  -37.1499
+  -37.9407
+  -38.8633
+  -39.5222
+  -40.8402
+  -41.6310];
 
 dayCountSum = zeros(180,360);
 dayEastGrad =  zeros(180,360);
@@ -41,8 +73,14 @@ else
     load('/Users/petercornillon/Dropbox/Data/MODIS_L2/gradient_stats_by_period/landMask')
 end
 
+yearsText = '';
+if length(yearsToProcess) == 1
+    yearsText = num2str(yearsToProcess);
+end
+
 filelist = dir('~/Dropbox/Data/MODIS_L2/gradient_stats_by_period/monthly_stats_*');
 
+numMonths = 0;
 for iFile=1:length(filelist)
     filename = [filelist(iFile).folder '/' filelist(iFile).name];
 
@@ -55,6 +93,8 @@ for iFile=1:length(filelist)
 
         switch temporalIntervals
             case 'Years'
+                numMonths = numMonths + 1;
+
                 dayCountSum = dayCountSum + ncread(filename, 'day_pixel_count');
                 dayEastGrad = dayEastGrad + ncread(filename, 'day_sum_eastward_gradient');
                 dayNorthGrad = dayNorthGrad + ncread(filename, 'day_sum_northward_gradient');
@@ -88,7 +128,10 @@ nightMeanEastGrad = nightEastGrad ./ nightCountSum;
 nightMeanNorthGrad = nightNorthGrad ./ nightCountSum;
 nightMeanGradMag = nightGradMagSum ./ nightCountSum;
 
-meanGradMag = (dayGradMagSum + nightGradMagSum) ./ (dayCountSum + nightCountSum);
+meanEastGrad = (dayEastGrad + nightEastGrad) ./ (dayCountSum + nightCountSum);
+meanNorthGrad = (dayNorthGrad + nightNorthGrad) ./ (dayCountSum + nightCountSum);
+
+meanEastwardGradient = (dayGradMagSum + nightGradMagSum) ./ (dayCountSum + nightCountSum);
 
 gradStructure.dayMeanEastGrad = dayMeanEastGrad;
 gradStructure.dayMeanNorthGrad = dayMeanNorthGrad;
@@ -105,7 +148,6 @@ gradStructure.nightGradMagHist = nightGradMagHist;
 
 %% Plot eastward and northward gradient images
 
-figNo = figNo + 1;
 figure(figNo)
 clf
 
@@ -132,7 +174,7 @@ caxis([-0.03 0.03])
 xlabel('Longitude')
 ylabel('Latitude')
 set(gca, fontsize=axisFontSize, ydir="normal")
-title('Daytime Eastward Gradient', fontsize=titleFontSize)
+title(['Daytime Eastward Gradient (' yearsText ')'], fontsize=titleFontSize)
 
 subplot(222)
 if plot_with_imagesc
@@ -157,7 +199,7 @@ caxis([-0.03 0.03])
 xlabel('Longitude')
 ylabel('Latitude')
 set(gca, fontsize=axisFontSize, ydir="normal")
-title('Daytime Northward Gradient', fontsize=titleFontSize)
+title(['Daytime Northward Gradient (' yearsText ')'], fontsize=titleFontSize)
 
 subplot(223)
 if plot_with_imagesc
@@ -182,7 +224,7 @@ caxis([-0.03 0.03])
 xlabel('Longitude')
 ylabel('Latitude')
 set(gca, fontsize=axisFontSize, ydir="normal")
-title('Nighttime Eastward Gradient', fontsize=titleFontSize)
+title(['Nighttime Eastward Gradient (' yearsText ')'], fontsize=titleFontSize)
 
 subplot(224)
 if plot_with_imagesc
@@ -208,12 +250,11 @@ caxis([-0.03 0.03])
 xlabel('Longitude')
 ylabel('Latitude')
 set(gca, fontsize=axisFontSize, ydir="normal")
-title('Nighttime Northward Gradient', fontsize=titleFontSize)
+title(['Nighttime Northward Gradient (' yearsText ')'], fontsize=titleFontSize)
 
 %% Plot gradient magnitudes.
 
-figNo = figNo + 1;
-figure(figNo)
+figure(figNo+1)
 clf
 subplot(211)
 if plot_with_imagesc
@@ -257,12 +298,11 @@ caxis([0 0.15])
 xlabel('Longitude')
 ylabel('Latitude')
 set(gca, fontsize=axisFontSize, ydir="normal")
-title('Nighttime Gradient Magnitude', fontsize=titleFontSize)
+title(['Nighttime Gradient Magnitude (' yearsText ')'], fontsize=titleFontSize)
 
 %% Plot day and night gradient magnitudes together, first with pcolor
 
-figNo = figNo + 1;
-figure(figNo)
+figure(figNo+2)
 clf
 
 if plot_with_imagesc
@@ -284,16 +324,15 @@ caxis([0 0.15])
 xlabel('Longitude')
 ylabel('Latitude')
 set(gca, fontsize=axisFontSize, ydir="normal")
-title('Mean Gradient Magnitude', fontsize=titleFontSize)
+title(['Mean Gradient Magnitude (' yearsText ')'], fontsize=titleFontSize)
 
 %% Plot the combined number of counts from day and night
 
-figNo = figNo + 1;
-figure(figNo)
+figure(figNo+3)
 clf
 
 if plot_with_imagesc
-    imagesc(lonVector, latVector, log10((dayCountSum + nightCountSum)/length(filelist)))
+    imagesc(lonVector, latVector, log10((dayCountSum + nightCountSum)/(2 * numMonths)))
     colormap("jet")
 else
     % Set land areas to NaN
@@ -308,11 +347,79 @@ else
 
     % Set the color scale to be a couple of orders of orders of magnitude
     % larger than the largest value.
-    maxVal = max(log10((dayCountSum + nightCountSum)/length(filelist)), [], 'all', 'omitnan');
+    maxVal = max(log10((dayCountSum + nightCountSum)/(2 * numMonths)), [], 'all', 'omitnan');
     caxis([0 floor(maxVal)+2])
 end
 colorbar
 xlabel('Longitude')
 ylabel('Latitude')
 set(gca, fontsize=axisFontSize, ydir="normal")
-title('log_{10} of Monthly Average of Number of Day + Night Counts', fontsize=titleFontSize)
+title(['log_{10} of Monthly Average of Number of Day + Night Counts (' yearsText ')'], fontsize=titleFontSize)
+
+%% Plot day and night eastward gradients together.
+figure(figNo+4)
+clf
+
+if plot_with_imagesc
+    imagesc(lonVector, latVector, meanEastGrad)
+    colormap("jet")
+else
+    % Set land areas to NaN
+    maskedData = meanEastGrad;
+    maskedData(inLand) = NaN;
+    pcolor(lonVector, latVector, maskedData)
+    shading flat
+    cmap = jet(256); % Example colormap (jet) with 256 colors
+    colormap(cmap);
+    % Set color for NaN values (gray) using the axes background color
+    set(gca, 'Color', [0.7 0.7 0.7]); % Gray background for NaN values
+end
+colorbar
+caxis([0 0.15])
+xlabel('Longitude')
+ylabel('Latitude')
+set(gca, fontsize=axisFontSize, ydir="normal")
+title(['Mean Eastward Gradient (' yearsText ')'], fontsize=titleFontSize)
+
+%% Plot day and night northward gradients together.
+figure(figNo+5)
+clf
+
+if plot_with_imagesc
+    imagesc(lonVector, latVector, meanNorthGrad)
+    colormap("jet")
+else
+    % Set land areas to NaN
+    maskedData = meanNorthGrad;
+    maskedData(inLand) = NaN;
+    pcolor(lonVector, latVector, maskedData)
+    shading flat
+    cmap = jet(256); % Example colormap (jet) with 256 colors
+    colormap(cmap);
+    % Set color for NaN values (gray) using the axes background color
+    set(gca, 'Color', [0.7 0.7 0.7]); % Gray background for NaN values
+end
+colorbar
+caxis([0 0.15])
+xlabel('Longitude')
+ylabel('Latitude')
+set(gca, fontsize=axisFontSize, ydir="normal")
+title(['Mean Northward Gradient (' yearsText ')'], fontsize=titleFontSize)
+
+%% Additional annotation
+
+% Add meander peak locations from 2008 to mean grad mag for day + night as
+% well as in the daytime eastward gradient.
+
+figure(figNo)
+subplot(221)
+hold on
+plot(mpx, mpy, 'ok', markerfacecolor='k', markersize=5)
+axis([10 100 -70 -30])
+
+for iPlotIndex=[2 4 5]
+    figure(figNo+iPlotIndex)
+    hold on
+    plot(mpx, mpy, 'ok', markerfacecolor='r', markersize=5)
+end
+
